cf382b3671562c33c8513f6aeddb534fe5d60038
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-18 16:54:30 -0700

    create toast store

---
+++ widget/src/stores/ToastStore.ts
@@ -0,0 +1,17 @@
+import { writable } from 'svelte/store'
+
+function createToast() {
+  const { subscribe, set } = writable(null)
+
+  return {
+    subscribe,
+    pop: ({ msg = '', error = false, warning = false, success = false }) => {
+      set({ msg, error, warning, success })
+      setTimeout(() => {
+        set(null)
+      }, 2500)
+    },
+  }
+}
+
+export const toaster = createToast()
