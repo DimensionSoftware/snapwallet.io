5b329d20a9938ef693038460c30b60236dd96d87
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-18 16:55:05 -0700

    wip: style modal, adds toaster

--- widget/src/App.svelte
+++ widget/src/App.svelte
@@ -1,30 +1,61 @@
 <script lang="ts">
+  import Toast from './components/Toast.svelte'
+  import { toaster } from './stores/ToastStore'
+
+  // Querystring provided props, see main.ts.
   export let name: string
+  export let intent: 'buy' | 'sell'
+  export let apiKey: string
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>The Flux cryptocurrency widget.</p>
-</main>
+<div class="modal">
+  <div id="modal-body">
+    <p>Product name: {name}</p>
+    <p>Intent: {intent}</p>
+    <p>API Key: {apiKey}</p>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    <button
+      on:click={() => {
+        toaster.pop({ msg: 'Success', success: true })
+      }}>Toaster</button
+    >
+    <Toast />
+  </div>
+</div>
+
+<style lang="scss">
+  @import './styles/_vars.scss';
+
+  :global(body, html, main, div) {
+    color: $textColor1;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    position: relative;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  .modal {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: $modalContainerBackgroundColor;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  #modal-body {
+    width: 320px;
+    height: 540px;
+    background-color: $modalBackgroundColor;
+    padding: 1rem;
+    border-radius: 1rem;
+    overflow: hidden;
+    overflow-y: scroll;
   }
 </style>
