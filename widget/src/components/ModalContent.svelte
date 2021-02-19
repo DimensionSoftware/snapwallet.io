11fd74f6cf9c79d73ec88c6e9b8a9ec7aa66b16c
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-18 18:50:02 -0700

    add modal components

---
+++ widget/src/components/ModalContent.svelte
@@ -0,0 +1,13 @@
+<div class="modal-content">
+  <slot />
+</div>
+
+<style lang="scss">
+  .modal-content {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+</style>
---
+++ widget/src/components/ModalFooter.svelte
@@ -0,0 +1,12 @@
+<div class="modal-footer">
+  <slot />
+</div>
+
+<style lang="scss">
+  .modal-footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 10%;
+  }
+</style>
