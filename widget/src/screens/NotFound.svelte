b69d727dc92aee501667156a278ad09fdd444f5a
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-18 18:52:25 -0700

    wrap in modal body

--- widget/src/screens/NotFound.svelte
+++ widget/src/screens/NotFound.svelte
@@ -1,1 +1,8 @@
-<div>Oops, we're sorry. The page requested is missing.</div>
+<script>
+  import ModalBody from '../components/ModalBody.svelte'
+  import ModalContent from '../components/ModalContent.svelte'
+</script>
+
+<ModalContent>
+  <ModalBody>Oops, we're sorry. The page requested is missing.</ModalBody>
+</ModalContent>
