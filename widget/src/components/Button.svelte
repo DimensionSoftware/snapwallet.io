884b4ac288031054035db6429224e72dbe5ff616
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-18 18:49:54 -0700

    add button component

---
+++ widget/src/components/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+</script>
+
+<button on:click={() => dispatch('click')}><slot /></button>
+
+<style lang="scss">
+  @import '../styles/_vars.scss';
+
+  button {
+    min-height: 40px;
+    width: 100%;
+    background-color: $greyBackground1;
+    border-radius: 0.5rem;
+    color: white;
+    cursor: pointer;
+    text-transform: capitalize;
+  }
+</style>
