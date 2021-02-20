1596a227c832dace847490ca642541107e5a88c8
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-20 16:06:45 -0700

    wip: card

---
+++ widget/src/components/cards/Card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="card-container">
+  <slot />
+</div>
+
+<style lang="scss">
+  @import '../../styles/_vars.scss';
+
+  .card-container {
+    border: 1px solid $textColor4;
+    min-height: 3rem;
+    width: 100%;
+    border-radius: 0.5rem;
+    padding: 0.2rem;
+  }
+</style>
