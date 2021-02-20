5ac12c23419ef5041a14ca8996424fbf14b9b07c
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-20 16:06:59 -0700

    wip: home

--- widget/src/screens/Home.svelte
+++ widget/src/screens/Home.svelte
@@ -7,9 +7,15 @@
   import Button from '../components/Button.svelte'
   import { userStore } from '../stores/UserStore'
   import Input from '../components/inputs/Input.svelte'
+  import PopupSelector from '../components/inputs/PopupSelector.svelte'
+  import CryptCard from '../components/cards/CryptoCard.svelte'
+  import Card from '../components/cards/Card.svelte'
 
+  let selectorVisible = false
   const handleClick = () => {
-    toaster.pop({ msg: 'Success', success: true })
+    // toaster.pop({ msg: 'Success', success: true })
+    // TODO: remove
+    selectorVisible = true
   }
 </script>
 
@@ -17,6 +23,17 @@
   <ModalBody>
     <IntentSelector />
     <Input label="Name" placeholder="Name" />
+    <Card>Stuff</Card>
+    <PopupSelector
+      on:close={() => (selectorVisible = false)}
+      visible={selectorVisible}
+      headerTitle="Select Cryptocurrency"
+    >
+      <CryptCard name="Bitcoin" ticker="BTC" />
+      <CryptCard name="Ethereum" ticker="ETH" />
+      <CryptCard name="Tether" ticker="USDT" />
+      <CryptCard name="USDC" ticker="USDC" />
+    </PopupSelector>
   </ModalBody>
   <ModalFooter>
     <Button on:click={handleClick}>{$userStore.intent}</Button></ModalFooter
