c657805f215fc0c40c8b7c325ec9612861036437
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-20 19:00:38 -0700

    add label component

---
+++ widget/src/components/inputs/Label.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let hidden: boolean
+</script>
+
+<div class="input-label">
+  {#if !hidden}
+    <slot />
+  {/if}
+</div>
+
+<style lang="scss">
+  .input-label {
+    font-size: 0.7rem;
+    height: 25%;
+    font-weight: 600;
+  }
+</style>
