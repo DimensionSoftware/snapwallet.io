d6138dff59f5279fcdf73536e4a1292a8294f617
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-20 17:29:59 -0700

    add txn store

---
+++ widget/src/stores/TransactionStore.ts
@@ -0,0 +1,22 @@
+import { writable } from 'svelte/store'
+import { IAsset, TransactionMediums } from '../types'
+
+const createStore = () => {
+  const { subscribe, update } = writable({
+    inMedium: TransactionMediums.ACH,
+    outMedium: TransactionMediums.BLOCKCHAIN,
+    sourceId: null,
+    sourceCurrency: { name: 'USD', ticker: 'USD' },
+    destinationCurrency: { name: 'Bitcoin', ticker: 'BTC' },
+  })
+
+  return {
+    subscribe,
+    setSourceCurrency: (sourceCurrency: IAsset) =>
+      update((s) => ({ ...s, sourceCurrency })),
+    setDestinationCurrency: (destinationCurrency: IAsset) =>
+      update((s) => ({ ...s, destinationCurrency })),
+  }
+}
+
+export const transactionStore = createStore()
