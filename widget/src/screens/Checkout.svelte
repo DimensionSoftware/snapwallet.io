05a983c29ebbec415971810b268bb42d8bc9ddea
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-25 19:11:31 -0700

    use modal header in checkout

--- widget/src/screens/Checkout.svelte
+++ widget/src/screens/Checkout.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
-  import {pop} from 'svelte-spa-router'
   import ModalBody from '../components/ModalBody.svelte'
   import ModalContent from '../components/ModalContent.svelte'
   import ModalFooter from '../components/ModalFooter.svelte'
   import Button from '../components/Button.svelte'
   import Input from '../components/inputs/Input.svelte'
   import Label from '../components/inputs/Label.svelte'
+  import ModalHeader from '../components/ModalHeader.svelte'
 
-  let animation = "left"
+  let animation = 'left'
 </script>
 
 <ModalContent {animation}>
-  <svg on:click={pop} viewBox="0 0 20 11" fill="none" class="back-arrow"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 5.267L14.343 0l-1.414 1.217 3.243 3.01H0v1.958h16.172L12.929 9.53 14.343 11 20 5.267z" fill="black"></path></svg>
   <ModalBody>
+    <ModalHeader />
     <h3>Enter Your Email</h3>
-    <Input inputmode="email" autocapitalize="none" autocomplete="on" type="email" placeholder="your@email.address" />
+    <Input
+      inputmode="email"
+      autocapitalize="none"
+      autocomplete="on"
+      type="email"
+      placeholder="your@email.address"
+    />
   </ModalBody>
   <ModalFooter>
     <Button>Continue</Button>
@@ -26,14 +32,5 @@
 
   h3 {
     margin: 25% 0 0;
-  }
-
-  .back-arrow {
-    height: 10px;
-    transform: rotate(180deg);
-    cursor: pointer;
-    position: absolute;
-    left: 0;
-    top: 10px;
   }
 </style>
