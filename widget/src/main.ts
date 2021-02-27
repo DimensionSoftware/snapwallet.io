996eed8a7253b18afaf06943c357ca70ec4feb93
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-27 10:16:29 -0700

    remove api call from main

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,18 +1,5 @@
 import App from './App.svelte'
 
-import { FluxApi, createConfiguration, ServerConfiguration } from 'api-client'
-
-async function testyMcAPI(): Promise<void> {
-  const api = new FluxApi(
-    createConfiguration({
-      baseServer: new ServerConfiguration('http://localhost:5100', {}),
-    }),
-  )
-
-  console.log(await api.fluxPricingData())
-}
-testyMcAPI()
-
 const queryParams = new URLSearchParams(window.location.search)
 
 const app = new App({
