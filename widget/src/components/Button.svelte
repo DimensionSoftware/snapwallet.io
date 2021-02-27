18eba64150f7bdb984832cb1f32fbbf67da560a5
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-26 19:52:17 -0700

    update button vars

--- widget/src/components/Button.svelte
+++ widget/src/components/Button.svelte
@@ -12,20 +12,20 @@
   button {
     min-height: 50px;
     width: 100%;
-    background-color: $themeColor;
+    background-color: var(--theme-color);
     border-radius: 0.5rem;
     border: none;
-    border-bottom: 2px solid darken($themeColor, 10%);
+    border-bottom: 2px solid var(--theme-color-darkened);
     color: white;
     cursor: pointer;
     text-transform: uppercase;
-    box-shadow: 0 6px 6px rgba($themeColor, 0.2);
+    box-shadow: 0 6px 6px var(--theme-button-shadow-color);
     letter-spacing: 2px;
     margin: 0;
     font-weight: bold;
     &:active,
     &:focus {
-      background-color: lighten($themeColor, 5%);
+      background-color: var(--theme-color-lightened);
     }
     &:disabled {
       opacity: 0.9;
