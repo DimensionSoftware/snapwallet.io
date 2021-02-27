0c66f0c9e61984bb9d0217974019575b3628407e
Chris Almeida <thechrisalmeida@gmail.com>
2021-02-27 15:16:30 -0700

    separate total as component

---
+++ widget/src/components/TotalContainer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { isValidNumber } from '../util'
+  import { transactionStore } from '../stores/TransactionStore'
+
+  export let isDestination: boolean
+  export let rate: number = 0.0
+
+  $: precision = isDestination ? 8 : 2
+
+  $: ticker = isDestination
+    ? $transactionStore.destinationCurrency.ticker
+    : $transactionStore.sourceCurrency.ticker
+
+  $: total = isValidNumber(rate) ? rate : 0
+</script>
+
+<div class="total-container">
+  ~ {total.toFixed(precision)}
+  {ticker}
+</div>
+
+<style lang="scss">
+  .total-container {
+    display: flex;
+    justify-content: flex-end;
+    color: var(--theme-text-color);
+    font-weight: 500;
+    font-size: 0.9rem;
+  }
+</style>
