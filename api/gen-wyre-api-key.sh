4c74e639331c52a5ecb90986af611a40ba11758b
Matthew Elder <melder@hellotech.com>
2021-03-01 07:38:15 -0800

    remove B.S.

--- api/gen-wyre-api-key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xeu
-
-API_KEY=$(date +%s | sha256sum | base64 | head -c 35)
-echo "API KEY: $API_KEY"
-
-curl -XPOST \
-  -H "Content-Type: application/json" \
-  -d "{\"secretKey\": \"$API_KEY\"}" \
-  "https://api.testwyre.com/v2/sessions/auth/key?secretKey=$WYRE_SECRET_KEY"
