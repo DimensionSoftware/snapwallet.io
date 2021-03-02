167d8973485d3bb8b1bd73de5000109c4236901c
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-02 09:52:22 -0700

    handle code resend, toggle button text

--- widget/src/screens/VerifyOTP.svelte
+++ widget/src/screens/VerifyOTP.svelte
@@ -15,21 +15,69 @@
   let animation = 'left'
   let code = ''
   let isMakingRequest = false
+  let isSendingEmail = false
+
+  const resendEmail = async () => {
+    Logger.debug('Resending email')
+    isSendingEmail = true
+    return await window.API().fluxOneTimePasscode({
+      emailOrPhone: $userStore.emailAddress,
+    })
+  }
+
+  const verifyOTP = async () => {
+    Logger.debug('Verifying using OTP code:', code)
+    return await window.API().fluxOneTimePasscodeVerify({
+      code,
+      emailOrPhone: $userStore.emailAddress,
+    })
+  }
+
+  const handleResend = async () => {
+    isMakingRequest = true
+
+    try {
+      await resendEmail()
+      Logger.debug('Email sent')
+      setTimeout(() => {
+        code = ''
+        toaster.pop({
+          msg: 'Success! Please check your email inbox.',
+          success: true,
+        })
+      }, 600)
+    } catch (e) {
+      Logger.error(e)
+      // TODO: move error messages to the server
+      let msg = 'An unknown error occurred. Please try again later.'
+      const code = e.body?.code
+
+      if ([3].includes(code)) {
+        msg = 'The email provided was invalid. Please re-enter and try again.'
+      }
+
+      toaster.pop({
+        msg,
+        error: true,
+      })
+      setTimeout(() => {
+        push('#/checkout')
+      }, 1700)
+    } finally {
+      setTimeout(() => {
+        isSendingEmail = false
+        isMakingRequest = false
+      }, 1000)
+    }
+  }
 
   const handleNextStep = async () => {
     isMakingRequest = true
-    const c = code
-    Logger.debug('Verifying using OTP code:', c)
 
     try {
-      const response: OneTimePasscodeVerifyResponse = await window
-        .API()
-        .fluxOneTimePasscodeVerify({
-          emailOrPhone: $userStore.emailAddress,
-          code: c,
-        })
+      const response = await verifyOTP()
       Logger.debug('LOGGED IN:', response.user)
-      setTimeout(() => push('#/profile'), 600)
+      setTimeout(() => push('#/profile'), 700)
     } catch (e) {
       Logger.error(e)
       // TODO: move error messages to the server
@@ -76,14 +124,33 @@
         }}
       />
     </Label>
+    <div class="resend">
+      Didn't get an email? <a on:click={handleResend}>Resend Code</a>
+    </div>
   </ModalBody>
   <ModalFooter>
-    <Button disabled={isMakingRequest} on:click={handleNextStep}
-      >Verify Email</Button
-    >
+    <Button disabled={isMakingRequest} on:click={handleNextStep}>
+      {#if isMakingRequest && !isSendingEmail}
+        Verifying Code...
+      {:else if isMakingRequest && isSendingEmail}
+        Resending Email...
+      {:else}
+        Verify Email
+      {/if}
+    </Button>
   </ModalFooter>
 </ModalContent>
 
 <style lang="scss">
   @import '../styles/_vars.scss';
+  .resend {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    margin-top: -1rem;
+    & > a {
+      margin-left: 0.5rem;
+    }
+  }
 </style>
