739cad7aa17c543330211c5c1b86ac91645a07b2
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-05 19:38:24 -0700

    update supported currencies

--- widget/src/constants.ts
+++ widget/src/constants.ts
@@ -1,5 +1,15 @@
 export const supportedCurrencyPairs = {
-  wyre: ['USD_BTC', 'USD_ETH', 'USD_USDC', 'USD_USDT'],
+  wyre: [
+    'USD_BTC',
+    'USD_ETH',
+    'USD_USDC',
+    'USD_USDT',
+    'USD_DAI',
+    'USD_MKR',
+    'USD_GUSD',
+    'USD_PAX',
+    'USD_LINK',
+  ],
 }
 
 export const JWT_SESSION_KEY = '__FLUX_SESSION'
