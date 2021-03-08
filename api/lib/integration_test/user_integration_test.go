4b0bb0f4d03d87362b804d6773764a51e410249d
Matthew Elder <melder@hellotech.com>
2021-03-07 23:51:16 -0800

    integration testzor

--- api/lib/integration_test/user_integration_test.go
+++ api/lib/integration_test/user_integration_test.go
@@ -20,11 +20,19 @@ func Test_User_Lifecycle(t *testing.T) {
 	}
 
 	email := faker.Internet().SafeEmail()
+
 	u, err := s.Db.GetOrCreateUser(ctx, onetimepasscode.LoginKindEmail, email)
 	a.NoError(err)
 
 	u2, err := s.Db.GetOrCreateUser(ctx, onetimepasscode.LoginKindEmail, email)
 	a.NoError(err)
-
 	a.Equal(u.ID, u2.ID, "the same email should correlate with the same user id on second visit")
+	a.Equal(u.Email, u2.Email)
+	a.True(u.CreatedAt.Equal(u2.CreatedAt), "u.CreatedAt.Equal(u2.CreatedAt)")
+
+	u3, err := s.Db.GetUserByID(ctx, u.ID)
+	a.NoError(err, "fetch user by id needs to work and should return the same results as an email lookup")
+	a.Equal(u.ID, u3.ID)
+	a.Equal(u.Email, u3.Email)
+	a.True(u.CreatedAt.Equal(u3.CreatedAt), "u.CreatedAt.Equal(u3.CreatedAt)")
 }
