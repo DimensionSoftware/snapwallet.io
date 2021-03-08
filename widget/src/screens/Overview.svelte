5e51a725a76183284f6b4ec17b8428b2d9046716
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-08 15:14:15 -0700

    wiphp add overview screen

---
+++ widget/src/screens/Overview.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import ModalContent from '../components/ModalContent.svelte'
+    import ModalBody from '../components/ModalBody.svelte'
+    import ModalHeader from '../components/ModalHeader.svelte'
+    import ModalFooter from '../components/ModalFooter.svelte'
+</script>
+
+
+<ModalContent>
+  <ModalBody>
+      <ModalHeader>Checkout Overview</ModalHeader>
+  </ModalBody>
+  <ModalFooter />
+</ModalContent>
+  
+
+<style lang="scss">
+
+</style>
