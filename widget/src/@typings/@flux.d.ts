0356d2e50e6fc1d70b9ea570de997aac3009798f
Matthew Elder <melder@hellotech.com>
2021-03-13 11:23:51 -0700

    fix stupid __ENV errors

--- widget/src/@typings/@flux.d.ts
+++ widget/src/@typings/@flux.d.ts
@@ -1,14 +1,16 @@
 import type { PromiseFluxApi } from 'api-client/dist/types/PromiseAPI'
 
-declare var __ENV: {
-  API_BASE_URL: string
-  DEBUG: string
-  [k: string]: string
-}
+
 
 declare global {
   interface Window {
     Plaid: any
     API?: PromiseFluxApi
+  }
+
+  declare var __ENV: {
+    API_BASE_URL: string
+    DEBUG: string
+    [k: string]: string
   }
 }
