aa3f34c137e07bef256f9ac3f36b44f9e2ee87ed
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-16 16:57:17 -0600

    add addr 2 to split up view

---
+++ widget/src/screens/Address2.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import { push } from 'svelte-spa-router'
+  import ModalBody from '../components/ModalBody.svelte'
+  import ModalContent from '../components/ModalContent.svelte'
+  import ModalFooter from '../components/ModalFooter.svelte'
+  import Button from '../components/Button.svelte'
+  import Input from '../components/inputs/Input.svelte'
+  import Label from '../components/inputs/Label.svelte'
+  import ModalHeader from '../components/ModalHeader.svelte'
+  import { userStore } from '../stores/UserStore'
+  import { onEnterPressed } from '../util'
+
+  let animation = 'left'
+  let isSubmittingProfile = false
+
+  const handleNextStep = async () => {
+    try {
+      isSubmittingProfile = true
+      await window.API.fluxSaveProfileData({
+        address: $userStore.address,
+      })
+      // TODO: push(somewhere)
+    } finally {
+      setTimeout(() => (isSubmittingProfile = false), 800)
+    }
+  }
+
+  const onKeyDown = (e: Event) => {
+    onEnterPressed(e, handleNextStep)
+  }
+</script>
+
+<svelte:window on:keydown={onKeyDown} />
+
+<ModalContent {animation}>
+  <ModalBody>
+    <ModalHeader>Where do you live?</ModalHeader>
+    <Label label="Country">
+      <Input placeholder="Country" defaultValue={$userStore.address.country} />
+    </Label>
+    <Label label="State">
+      <Input placeholder="State" defaultValue={$userStore.address.state} />
+    </Label>
+    <Label label="Postal Code">
+      <Input
+        placeholder="Postal Code"
+        defaultValue={$userStore.address.postalCode}
+      />
+    </Label>
+  </ModalBody>
+  <ModalFooter>
+    <Button on:click={handleNextStep}
+      >{isSubmittingProfile ? 'Submitting...' : 'Done'}</Button
+    >
+  </ModalFooter>
+</ModalContent>
+
+<style lang="scss">
+  @import '../styles/_vars.scss';
+  label {
+    size: smaller;
+  }
+</style>
