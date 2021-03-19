89dfaf4f850771a9073f7a3ea843c2f9e45dd03c
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-19 12:48:52 -0600

    move crypto selector

---
+++ widget/src/components/selectors/CryptoSelector.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  import CryptoCard from '../cards/CryptoCard.svelte'
+  import Label from '../inputs/Label.svelte'
+  import PopupSelector from '../inputs/PopupSelector.svelte'
+  const dispatch = createEventDispatcher()
+
+  const cryptoCurrencies = [
+    { name: 'Bitcoin', ticker: 'BTC', popular: true },
+    { name: 'Ethereum', ticker: 'ETH', popular: true },
+    { name: 'USDC', ticker: 'USDC' },
+    { name: 'Tether', ticker: 'USDT', popular: true },
+    { name: 'DAI', ticker: 'DAI' },
+    { name: 'MakerDAO', ticker: 'MKR' },
+    { name: 'Gemini Dollar', ticker: 'GUSD' },
+    { name: 'Paxos Standard', ticker: 'PAX' },
+    { name: 'Link', ticker: 'LINK' },
+  ]
+
+  export let visible = false
+</script>
+
+<PopupSelector
+  {visible}
+  on:close={() => dispatch('close')}
+  headerTitle="Select Currency"
+>
+  <div class="scroll selector-container">
+    <h5>Popular</h5>
+    {#each cryptoCurrencies.filter(c => c.popular) as cryptoCurrency (cryptoCurrency.ticker)}
+      <div style="margin: 0.5rem 0">
+        <Label>
+          <CryptoCard
+            on:click={() => dispatch('close')}
+            crypto={cryptoCurrency}
+          />
+        </Label>
+      </div>
+    {/each}
+    <h5 style="margin-top: 1.25rem">All</h5>
+    {#each cryptoCurrencies.filter(c => !c.popular) as cryptoCurrency (cryptoCurrency.ticker)}
+      <div style="margin: 0.5rem 0">
+        <Label>
+          <CryptoCard
+            on:click={() => dispatch('close')}
+            crypto={cryptoCurrency}
+          />
+        </Label>
+      </div>
+    {/each}
+  </div>
+</PopupSelector>
+
+<style lang="scss">
+  @import '../../styles/selectors.scss';
+</style>
