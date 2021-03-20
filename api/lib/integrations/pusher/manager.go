c0ceb760d147bfdec2aefcc247ea9adb914f8f84
Matthew Elder <melder@hellotech.com>
2021-03-20 08:18:08 -0600

    define pusher messages

---
+++ api/lib/integrations/pusher/manager.go
@@ -0,0 +1,30 @@
+package pusher
+
+import (
+	"time"
+
+	"github.com/khoerling/flux/api/lib/db/models/user"
+	"github.com/pusher/pusher-http-go"
+)
+
+type Manager struct {
+	Pusher *pusher.Client
+}
+
+type MessageKind string
+
+const (
+	MessageKindWyreAccountUpdated       MessageKind = "WYRE_ACCOUNT_UPDATED"
+	MessageKindWyrePaymentMethodUpdated MessageKind = "WYRE_PAYMENT_METHOD_UPDATED"
+	MessageKindProfileStatusUpdated     MessageKind = "PROFILE_STATUS_UPDATED"
+)
+
+type Message struct {
+	Kind MessageKind `json:"kind"`
+	IDs  []string    `json:"ids"`
+	At   time.Time   `json:"at"`
+}
+
+func (m Manager) Send(userID user.ID, msg *Message) error {
+	return m.Pusher.Trigger(string(userID), string(msg.Kind), msg)
+}
