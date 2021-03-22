8d9d5ec87389867b76a5c203572319ef9bd8855e
Keith Hörling <keith@keiths-MacBook-Pro.local>
2021-03-22 09:37:21 -0700

    adds VerticalStepper

---
+++ widget/src/components/VerticalStepper.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let items = []
+</script>
+
+<ul class="vertical-stepper">
+  {#each items as item}
+    <li>
+      <svelte:component this={item} />
+    </li>
+  {/each}
+</ul>
+
+<style lang="scss">
+  @import '../styles/_vars.scss';
+  .vertical-stepper {
+    position: relative;
+    display: flex;
+  }
+</style>
