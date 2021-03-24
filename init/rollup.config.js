b0e0796ef03ac7b5ffe79173dc836f493b844443
Chris Almeida <thechrisalmeida@gmail.com>
2021-03-24 12:21:59 -0600

    add dotenv to rollup

--- init/rollup.config.js
+++ init/rollup.config.js
@@ -1,6 +1,10 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default {
   input: 'index.ts',
@@ -10,6 +14,12 @@ export default {
     name: 'init.js',
   },
   plugins: [
+    replace({
+      preventAssignment: true,
+      _ENV: JSON.stringify({
+        WIDGET_URL: process.env.WIDGET_URL,
+      }),
+    }),
     typescript(),
     resolve({
       jsnext: true,
