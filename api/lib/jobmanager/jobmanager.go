f681e91758715ce4642713ebf3280f68512b0cb2
Matthew Elder <melder@hellotech.com>
2021-03-25 01:16:16 -0600

    fin cycles

--- api/lib/jobmanager/jobmanager.go
+++ api/lib/jobmanager/jobmanager.go
@@ -1,17 +1,23 @@
 package jobmanager
 
 import (
+	"context"
+
 	"github.com/khoerling/flux/api/lib/db"
+	"github.com/khoerling/flux/api/lib/db/models/job"
 	"github.com/khoerling/flux/api/lib/integrations/pusher"
 	"github.com/khoerling/flux/api/lib/integrations/wyre"
-	"github.com/khoerling/flux/api/lib/interfaces/ijobpublisher"
 )
+
+type InnerJobPublisher interface {
+	PublishJob(context.Context, *job.Job) error
+}
 
 type Manager struct {
 	*db.Db
 	Pusher       *pusher.Manager
 	WyreManager  *wyre.Manager
-	JobPublisher ijobpublisher.JobPublisher
+	JobPublisher InnerJobPublisher
 }
 
 func (m Manager) GetDb() *db.Db {
@@ -26,6 +32,6 @@ func (m Manager) GetWyreManager() *wyre.Manager {
 	return m.WyreManager
 }
 
-func (m Manager) GetJobPublisher() ijobpublisher.JobPublisher {
+func (m Manager) GetJobPublisher() InnerJobPublisher {
 	return m.JobPublisher
 }
