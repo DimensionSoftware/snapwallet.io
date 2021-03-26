ab09b1eb8fecd0c57b7feb634a42759aab0fbf8b
Matthew Elder <melder@hellotech.com>
2021-03-26 10:50:44 -0600

    remove test cruft

--- api/cmd/grpcdevserver/main.go
+++ api/cmd/grpcdevserver/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"log"
 
 	"github.com/khoerling/flux/api/cmd/grpcserver/wire"
-	"github.com/khoerling/flux/api/lib/db/models/job"
 )
 
 const (
@@ -16,11 +14,6 @@ func main() {
 	s, err := wire.InitializeDevServer()
 	if err != nil {
 		panic(err)
-	}
-
-	err = s.JobPublisher.PublishJob(context.Background(), &job.Job{})
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	log.Println("> listening on port " + defaultAddress[1:])
