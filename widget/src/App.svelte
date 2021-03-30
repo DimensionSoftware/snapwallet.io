571f38061258c677a1217cddf059429ea2fe640b
Keith Hörling <keith@Slaptop.local>
2021-03-29 19:17:29 -0700

    adds and hooks <PreLogout /> up

--- widget/src/App.svelte
+++ widget/src/App.svelte
@@ -4,6 +4,7 @@
   import FaIcon from 'svelte-awesome'
   import { faLock } from '@fortawesome/free-solid-svg-icons'
   import Toast from './components/Toast.svelte'
+  import PreLogout from './components/PreLogout.svelte'
   import Home from './screens/Home.svelte'
   import SendOTP from './screens/SendOTP.svelte'
   import NotFound from './screens/NotFound.svelte'
@@ -33,23 +34,21 @@
   export let theme: object
   export let focus: boolean
 
+  $: isPreLogout = false
+
   // auth bits
   window.addEventListener('logout', () => {
     Logger.debug('viewer has logged out')
     userStore.setIsLoggedIn(false)
     push(Routes.ROOT)
     toaster.pop({
-      msg: 'You have been securely logged out',
+      msg: 'You have been securely logged out.',
       error: true,
     })
   })
   window.addEventListener('prelogout', () => {
-    const expirationEpoch = window.AUTH_MANAGER.getSessionExpiration()
-    const expirationDate = new Date(expirationEpoch)
-    // TODO: use ^^ for countdown input
-    Logger.debug(`viewer will be logged out at: ${expirationDate}`)
-    // TODO pop inactive session dialog up
-    // TODO either reactivate session via getAccessToken() or logout()
+    Logger.debug('viewer is prelogout')
+    isPreLogout = true
   })
 
   // Handler for routing condition failure
@@ -172,8 +171,8 @@
         ($location as Routes) !== Routes.VERIFY_OTP
       ) {
         // expired session, so-- automagically logout
-        // - handle re-routing elsewhere
-        return window.AUTH_MANAGER.logout()
+        window.AUTH_MANAGER.logout()
+        push(Routes.ROOT)
       }
 
       // show toast
@@ -194,9 +193,15 @@
 <svelte:window on:keydown={onKeyDown} on:mousedown={onMouseDown} />
 
 <div id="modal">
-  <div id="modal-body">
+  <div id="modal-body" class:blur={isPreLogout}>
     <Router on:conditionsFailed={routeConditionsFailed} {routes} />
     <Toast />
+    {#if isPreLogout}
+      <PreLogout
+        onClosed={() => (isPreLogout = false)}
+        isVisible={isPreLogout}
+      />
+    {/if}
   </div>
   <FaIcon class="lock" data={faLock} />
 </div>
@@ -310,6 +315,11 @@
     animation: scaleIn 0.25s var(--theme-ease-out-back);
     // Used by toast
     position: relative;
+    &.blur {
+      :global(.modal-content) {
+        filter: blur(5px);
+      }
+    }
   }
 
   @media screen and (max-width: 450px) {
---
+++ widget/src/components/PreLogout.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte'
+  import { scale } from 'svelte/transition'
+  import { expoOut } from 'svelte/easing'
+  import { focusFirstInput, onKeysPressed } from '../util'
+
+  export let isVisible: boolean = false
+  export let onClosed: Function
+
+  // for countdown timer
+  let _timer = null
+  $: expiresAfter = 0
+
+  const logout = () => {
+      // close, yielding animations
+      close()
+      setTimeout(() => window.AUTH_MANAGER.logout(), 100)
+    },
+    cont = () => {
+      // refresh token and close
+      window.AUTH_MANAGER.getAccessToken()
+      close()
+    },
+    close = () => {
+      isVisible = false
+      focusFirstInput()
+      onClosed()
+    },
+    keydown = e => {
+      // dismiss if esc pressed
+      if (onKeysPressed(e, ['Escape'])) close()
+    },
+    shouldClose = (e: MouseEvent) => {
+      // dismiss if clicked outside content
+      if ((e.target as HTMLElement).id === 'prelogout' && isVisible) close()
+    },
+    countdown = () => {
+      const diff: number =
+        +new Date(window.AUTH_MANAGER.getSessionExpiration()) - +new Date()
+      expiresAfter = Math.round(diff / 1000 / 60)
+    }
+
+  onMount(() => {
+    countdown()
+    _timer = setInterval(countdown, 1000)
+  })
+  onDestroy(() => {
+    clearInterval(_timer)
+  })
+</script>
+
+<svelte:window on:keydown={keydown} />
+
+<div
+  id="prelogout"
+  class="container"
+  class:visible={isVisible}
+  on:click={shouldClose}
+  out:scale={{ opacity: 0, start: 1.15, duration: 175, easing: expoOut }}
+>
+  <article>
+    <h3>Stay Signed In?</h3>
+    <p>
+      You will be securely signed out in <big>{expiresAfter}</big>
+      {expiresAfter > 1 ? 'minutes' : 'minute'}.
+    </p>
+    <div class="flex">
+      <button on:click={logout} class="logout">Logout</button>
+      <button on:click={cont} class="continue">Continue</button>
+    </div>
+  </article>
+</div>
+
+<style lang="scss">
+  @import '../styles/_vars.scss';
+  @import '../styles/animations.scss';
+  .container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    font-size: 1.2rem;
+    z-index: 101;
+    background: rgba(255, 255, 255, 0.7);
+    article {
+      position: relative;
+      margin: 0;
+      top: 20%;
+      background: var(--theme-modal-background);
+      border-radius: 3px;
+      padding: 1.5rem;
+      animation: slideUp 0.3s var(--theme-ease-out-back);
+      h3 {
+        margin: 0;
+      }
+      p {
+        font-size: 0.9rem;
+      }
+      .flex {
+        display: flex;
+        justify-content: start;
+        .continue {
+          cursor: pointer;
+          margin-left: 0.5rem;
+          color: var(--theme-text-color);
+          background: transparent;
+          border: none;
+        }
+        .logout {
+          cursor: pointer;
+          margin-right: 0.5rem;
+          background: var(--theme-color);
+          font-weight: 500;
+          padding: 0.25rem 1rem;
+          color: var(--theme-color-inverse);
+          border: none;
+        }
+      }
+    }
+  }
+</style>
