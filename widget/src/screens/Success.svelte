b9f2fb26773397aceb0b1f6f7ed5d29a7599255e
Keith Hörling <keith@Slaptop.local>
2021-03-30 16:30:35 -0700

    adds SVG animation to <Success /> screen

--- widget/src/screens/Success.svelte
+++ widget/src/screens/Success.svelte
@@ -6,8 +6,6 @@
   import ModalFooter from '../components/ModalFooter.svelte'
   import { transactionStore } from '../stores/TransactionStore'
   import { TransactionIntents } from '../types'
-  import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-  import FaIcon from 'svelte-awesome'
   import { ParentMessenger } from '../util/parent_messenger'
 
   $: ({ intent, wyrePreview } = $transactionStore)
@@ -21,11 +19,43 @@
   <ModalBody>
     <ModalHeader hideBackButton>Success</ModalHeader>
     <div class="icon-box">
-      <FaIcon scale="5" data={faCheckCircle} />
+      <svg
+        id="successAnimation"
+        class="animated"
+        xmlns="http://www.w3.org/2000/svg"
+        width="125"
+        height="125"
+        viewBox="0 0 70 70"
+      >
+        <path
+          id="successAnimationResult"
+          fill="#D8D8D8"
+          d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"
+        />
+        <circle
+          id="successAnimationCircle"
+          cx="35"
+          cy="35"
+          r="24"
+          stroke="#979797"
+          stroke-width="2"
+          stroke-linecap="round"
+          fill="transparent"
+        />
+        <polyline
+          id="successAnimationCheck"
+          stroke="#979797"
+          stroke-width="2"
+          points="23 34 34 43 47 27"
+          fill="transparent"
+        />
+      </svg>
     </div>
     <div class="text-center">
-      <p>Your {cryptoTicker} checkout is confirmed!</p>
-      <p>Please allow up to five (5) business days for your purchase to complete.</p>
+      <p>Your <b>{cryptoTicker}</b> checkout is confirmed!</p>
+      <p>
+        Please allow up to five (5) business days for your purchase to complete.
+      </p>
     </div>
   </ModalBody>
   <ModalFooter>
@@ -36,6 +66,9 @@
 <style lang="scss">
   @import '../styles/_vars.scss';
   @import '../styles/animations.scss';
+
+  $circle-length: 151px;
+  $check-length: 36px;
 
   .icon-box {
     width: 100%;
@@ -48,6 +81,83 @@
   }
   .text-center {
     margin: 0 1rem;
-    text-align: center;
+  }
+  b {
+    text-transform: uppercase;
+  }
+
+  @keyframes scaleAnimation {
+    0% {
+      opacity: 0;
+      transform: scale(3);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  @keyframes drawCircle {
+    0% {
+      stroke-dashoffset: $circle-length;
+    }
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+  @keyframes drawCheck {
+    0% {
+      stroke-dashoffset: $check-length;
+    }
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  @keyframes fadeIn {
+    0% {
+      transform: scale(1);
+      opacity: 0;
+    }
+    75% {
+      opacity: 1;
+      transform: scale(2);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  #successAnimationCircle {
+    stroke-dasharray: $circle-length $circle-length;
+    stroke: var(--theme-success-color);
+  }
+  #successAnimationCheck {
+    stroke-dasharray: $check-length $check-length;
+    stroke: var(--theme-success-color);
+  }
+  #successAnimationResult {
+    fill: var(--theme-success-color);
+    opacity: 0;
+  }
+  #successAnimation.animated {
+    animation: 0.9s var(--theme-ease-out-expo) 0s 1 both scaleAnimation;
+    #successAnimationCircle {
+      animation: 1s cubic-bezier(0.77, 0, 0.175, 1) 0s 1 both drawCircle,
+        0.3s var(--theme-ease-out-expo) 0.9s 1 both fadeOut;
+    }
+    #successAnimationCheck {
+      animation: 1s cubic-bezier(0.77, 0, 0.175, 1) 0s 1 both drawCheck,
+        0.3s var(--theme-ease-out-expo) 0.9s 1 both fadeOut;
+    }
+    #successAnimationResult {
+      animation: 0.3s var(--theme-ease-out-back) 0.8s both fadeIn;
+    }
   }
 </style>
