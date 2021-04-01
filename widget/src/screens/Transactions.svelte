ddc66d65b01ffbef7cc29d4676fffe5ad82540ab
Chris Almeida <thechrisalmeida@gmail.com>
2021-04-01 10:18:28 -0600

    keep txn padding even

--- widget/src/screens/Transactions.svelte
+++ widget/src/screens/Transactions.svelte
@@ -148,7 +148,7 @@
     width: 100%;
     align-self: center;
     margin-top: 0.5rem;
-    padding: 0.5rem 0.5rem 0 1rem;
+    padding: 0.5rem 1rem 0 1rem;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
