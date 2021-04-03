e49df20f409aaf5ad2108c299f3cb7d9f6dd7620
Chris Almeida <thechrisalmeida@gmail.com>
2021-04-02 22:56:46 -0600

    update eth

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -38,7 +38,7 @@ const app = new App({
       //   'https://lh3.googleusercontent.com/NpXUf_nwxn9yhHk_1AwFxRE7Mg2Lb7_rZoxKRuhf5Tca9MKm0Fh1MXuUAlJNJooO34l6YX3d-2MEZ1kpZvQ18JtrQbQw8CHnBdnRUV8=s992',
       videoURL:
         'https://mkpcdn.com/videos/d3a277f4e6f1212c900a1da4ec915aa9_675573.mp4',
-      destinationAmount: 0.0651,
+      destinationAmount: 18.1,
       destinationTicker: 'ETH',
       destinationAddress: '0xf636B6aA45C554139763Ad926407C02719bc22f7',
       title: 'The Crown (Patrick Mahomes)',
