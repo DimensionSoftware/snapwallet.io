6133d4f194fa6400a2875cfa8ef695199d970a78
Chris Almeida <thechrisalmeida@gmail.com>
2021-04-02 22:55:14 -0600

    use the crown

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -34,14 +34,14 @@ const app = new App({
     focus: config?.focus,
     wallets: config?.wallets || [],
     product: {
-      imageURL:
-        'https://lh3.googleusercontent.com/NpXUf_nwxn9yhHk_1AwFxRE7Mg2Lb7_rZoxKRuhf5Tca9MKm0Fh1MXuUAlJNJooO34l6YX3d-2MEZ1kpZvQ18JtrQbQw8CHnBdnRUV8=s992',
-      // videoURL:
-      //   'https://storage.opensea.io/files/cf469112444efdc46e076504b471c229.mp4',
+      // imageURL:
+      //   'https://lh3.googleusercontent.com/NpXUf_nwxn9yhHk_1AwFxRE7Mg2Lb7_rZoxKRuhf5Tca9MKm0Fh1MXuUAlJNJooO34l6YX3d-2MEZ1kpZvQ18JtrQbQw8CHnBdnRUV8=s992',
+      videoURL:
+        'https://mkpcdn.com/videos/d3a277f4e6f1212c900a1da4ec915aa9_675573.mp4',
       destinationAmount: 0.0651,
       destinationTicker: 'ETH',
       destinationAddress: '0xf636B6aA45C554139763Ad926407C02719bc22f7',
-      title: 'Ecomi Card 1.0 (Cryptocircuits)',
+      title: 'The Crown (Patrick Mahomes)',
     },
     theme: {
       /* NOTE: each attribute maps to a css variable which is prefixed by --theme- */
