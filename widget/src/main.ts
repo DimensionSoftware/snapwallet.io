d4a61e166c4b93011b2a6a8fce74a5b97406ada4
Chris Almeida <thechrisalmeida@gmail.com>
2021-04-06 18:42:43 -0600

    as any config

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -18,7 +18,7 @@ PusherUtil.setup()
 userStore.setIsLoggedIn(window.AUTH_MANAGER.viewerIsLoggedIn())
 
 const queryParams = new URLSearchParams(window.location.search)
-let config = {}
+let config = {} as any
 try {
   config = JSON.parse(decodeURI(queryParams.get('config')))
 } catch {
@@ -40,7 +40,7 @@ const app = new App({
     //   //   'https://lh3.googleusercontent.com/NpXUf_nwxn9yhHk_1AwFxRE7Mg2Lb7_rZoxKRuhf5Tca9MKm0Fh1MXuUAlJNJooO34l6YX3d-2MEZ1kpZvQ18JtrQbQw8CHnBdnRUV8=s992',
     //   videoURL:
     //     'https://mkpcdn.com/videos/d3a277f4e6f1212c900a1da4ec915aa9_675573.mp4',
-    //   destinationAmount: 18.1,
+    //   destinationAmount: 0.0004,
     //   destinationTicker: 'ETH',
     //   destinationAddress: '0xf636B6aA45C554139763Ad926407C02719bc22f7',
     //   title: 'The Crown (Patrick Mahomes)',
