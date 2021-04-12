56d98f844edc400a0c700d945e26b2d8009e0e9e
Matthew Elder <melder@hellotech.com>
2021-04-11 19:01:34 -0600

    first basic test

--- api/lib/auth/interceptor_test.go
+++ api/lib/auth/interceptor_test.go
@@ -1,10 +1,19 @@
 package auth
 
 import (
+	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_authenticateMethod(t *testing.T) {
-	//a := assert.New(t)
-	//a.NoError(err)
+func Test_JwtVerifier_authenticateMethod(t *testing.T) {
+	a := assert.New(t)
+	v := JwtVerifier{}
+
+	ctx := context.Background()
+	var err error
+
+	ctx, err = v.authenticateMethod(ctx, "/Flux/PricingData")
+	a.NoError(err)
 }
