b9b95e5a8da615f92d8183409522453d7acf9954
Matthew Elder <melder@hellotech.com>
2021-04-11 19:40:03 -0600

    status test table

--- api/lib/auth/interceptor_test.go
+++ api/lib/auth/interceptor_test.go
@@ -5,15 +5,47 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func Test_JwtVerifier_authenticateMethod(t *testing.T) {
-	a := assert.New(t)
-	v := JwtVerifier{}
+	tt := []struct {
+		desc           string
+		fullMethod     string
+		expectedStatus *status.Status
+	}{
+		{
+			desc:           "tc 1",
+			fullMethod:     "/Flux/PricingData",
+			expectedStatus: nil,
+		},
+		{
+			desc:           "tc 2",
+			fullMethod:     "/Flux/ViewerData",
+			expectedStatus: status.New(codes.Unauthenticated, "authentication not set"),
+		},
+	}
 
-	ctx := context.Background()
-	var err error
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
 
-	ctx, err = v.authenticateMethod(ctx, "/Flux/PricingData")
-	a.NoError(err)
+			a := assert.New(t)
+			v := JwtVerifier{}
+
+			ctx := context.Background()
+			var err error
+
+			ctx, err = v.authenticateMethod(ctx, tc.fullMethod)
+			if tc.expectedStatus == nil {
+				a.NoError(err)
+			} else {
+				s, ok := status.FromError(err)
+				a.True(ok)
+				a.Equal(tc.expectedStatus, s)
+			}
+		})
+	}
 }
