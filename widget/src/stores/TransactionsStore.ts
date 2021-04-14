6d6b5a0a1476606f8461cf8e3cdb60915a9ae697
Matthew Elder <melder@hellotech.com>
2021-04-14 15:33:52 -0600

    cleanup pagination code

--- widget/src/stores/TransactionsStore.ts
+++ widget/src/stores/TransactionsStore.ts
@@ -1,29 +1,27 @@
 import type { WyreTransfer, WyreTransfers } from 'api-client'
 import { writable } from 'svelte/store'
+import { reducePersonalInfoFields } from '../util/profiles'
 
 const createStore = ()  => {
   const { subscribe, update } = writable<WyreTransfer[]>([])
   return {
     subscribe,
     fetchUserTransactions: async (): Promise<WyreTransfer[]> => {
-      const out = []
-
-      let page = 0
-      let pagelen = -1
-      while(pagelen !== 0) {
-        const { transfers } = await window.API.fluxWyreGetTransfers(page.toString())
-        for (const xfer of transfers) {
-          out.push(xfer)
-        }
-        pagelen = transfers.length
-        page++
-      }
-
-      update(_ => out)
-
-      return out
+      const txns = await rFetchUserTransfers()
+      update(_ => txns)
+      return txns
     },
+   }
+}
+
+async function rFetchUserTransfers(startingAtPage = 0): Promise<WyreTransfer[]> {
+  const { transfers } = await window.API.fluxWyreGetTransfers(startingAtPage.toString())
+
+  if (transfers.length) {
+    return transfers.concat((await rFetchUserTransfers(startingAtPage + 1)))
   }
+
+  return []
 }
 
 export const transactionsStore = createStore()
