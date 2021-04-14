7726708f0a88c7a491baa50c0c51fbd6aa380de8
Matthew Elder <melder@hellotech.com>
2021-04-14 13:24:08 -0600

    implement pagination in fetchUserTransactions

--- widget/src/stores/TransactionsStore.ts
+++ widget/src/stores/TransactionsStore.ts
@@ -1,12 +1,27 @@
+import type { WyreTransfer, WyreTransfers } from 'api-client'
 import { writable } from 'svelte/store'
 
-const createStore = () => {
-  const { subscribe, update } = writable([])
+const createStore = ()  => {
+  const { subscribe, update } = writable<WyreTransfer[]>([])
   return {
     subscribe,
-    fetchUserTransactions: async () => {
-      const { transfers } = await window.API.fluxWyreGetTransfers()
-      return update(_ => transfers)
+    fetchUserTransactions: async (): Promise<WyreTransfer[]> => {
+      const out = []
+
+      let page = 0
+      let pagelen = -1
+      while(pagelen !== 0) {
+        const { transfers } = await window.API.fluxWyreGetTransfers(page.toString())
+        for (const xfer of transfers) {
+          out.push(xfer)
+        }
+        pagelen = transfers.length
+        page++
+      }
+
+      update(_ => out)
+
+      return out
     },
   }
 }
