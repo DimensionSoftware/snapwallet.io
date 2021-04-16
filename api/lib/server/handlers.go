8a8a7c8c7f4ae6000107d17bed04ebfdd2d38a0a
Matthew Elder <melder@hellotech.com>
2021-04-16 17:22:18 -0600

    forgot product title

--- api/lib/server/handlers.go
+++ api/lib/server/handlers.go
@@ -1358,6 +1358,7 @@ func (s *Server) WidgetGetShortUrl(ctx context.Context, req *proto.SnapWidgetCon
 				DestinationAmount:  req.Product.DestinationAmount,
 				DestinationTicker:  req.Product.DestinationTicker,
 				DestinationAddress: req.Product.DestinationAddress,
+				Title:              req.Product.Title,
 			},
 		},
 	}
