493a2b5ff92f9c0615091aa67a3fc8f188c45053
Chris Almeida <thechrisalmeida@gmail.com>
2021-04-19 10:30:28 -0600

    move supported assets to const

--- widget/src/constants.ts
+++ widget/src/constants.ts
@@ -98,3 +98,26 @@ export enum UserProfileFieldTypes {
   ACH_AUTH_FORM = 'K_ACH_AUTHORIZATION_FORM_DOC',
   UNKNOWN = 'K_UNKNOWN',
 }
+
+export const SUPPORTED_CRYPTOCURRENCY_ASSETS = [
+  { name: 'Aave', ticker: 'AAVE', color: '#EFB914' },
+  { name: 'Basic Attention Token', ticker: 'BAT', color: '#FF5000' },
+  { name: 'Binance USD', ticker: 'BUSD', color: '#EFB914' },
+  { name: 'Bitcoin', ticker: 'BTC', popular: true, color: '#F7931A' },
+  { name: 'Curve', ticker: 'CRV', color: '#EFB914' },
+  { name: 'Compound', ticker: 'COMP', color: '#00D395' },
+  { name: 'DAI', ticker: 'DAI', color: '#F4B731' },
+  { name: 'Ethereum', ticker: 'ETH', popular: true, color: '#627EEA' },
+  { name: 'Gemini Dollar', ticker: 'GUSD', color: '#00DCFA' },
+  { name: 'Link', ticker: 'LINK', color: '#2A5ADA' },
+  { name: 'MakerDAO', ticker: 'MKR', color: '#1AAB9B' },
+  { name: 'Paxos Standard', ticker: 'PAX', color: '#EDE708' },
+  { name: 'Stably Dollar', ticker: 'USDS', color: '#EFB914' },
+  { name: 'Synthetix', ticker: 'SNX', color: '#EFB914' },
+  { name: 'Tether', ticker: 'USDT', popular: true, color: '#26A17B' },
+  { name: 'UMA', ticker: 'UMA', color: '#FF4A4A' },
+  { name: 'USDC', ticker: 'USDC', color: '#2775C9' },
+  { name: 'Uniswap', ticker: 'UNI', popular: true, color: '#FF007A' },
+  { name: 'Wrapped Bitcoin', ticker: 'WBTC', color: '#323544' },
+  { name: 'Yearn.Finance', ticker: 'YFI', color: '#006AE3' },
+]
