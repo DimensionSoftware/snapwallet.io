820ed9ca4ab6619867a30ed0eb0a6a1260b62b45
Keith Hörling <keith@Slaptop-2.local>
2021-04-21 21:27:17 -0700

    adds web/button

---
+++ web/src/lib/Button.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+  export let id
+  export let disabled: boolean = false
+  export let isLoading: boolean = false
+  export let title: string = ''
+</script>
+
+<button
+  {id}
+  disabled={disabled || isLoading}
+  class:isLoading
+  on:mousedown={() => dispatch('mousedown')}
+  {title}
+>
+  <div class="lds-circle"><div /></div>
+  <slot /></button
+>
+
+<style lang="scss">
+  @import '../../../widget/src/styles/animations.scss';
+  button {
+    position: relative;
+    min-height: 50px;
+    width: 100%;
+    background: transparent;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-radius: 0.5rem;
+    color: var(--theme-button-text-color);
+    cursor: pointer;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    margin: 0;
+    font-weight: bold;
+    transition: background 0.2s ease-in 0.05s;
+    z-index: 10;
+    &:hover {
+      background: var(--theme-button-color);
+      border-color: var(--theme-button-color);
+      box-shadow: 0 2px 2px 1px rgba(0, 0, 0, 0.1);
+      transition: none;
+    }
+    &:active,
+    &:focus {
+      transition: none;
+      background: var(--theme-button-color);
+      animation: infocus 0.35s;
+      animation-timing-function: var(--theme-ease-out-back);
+    }
+    &:disabled {
+      animation: infocus 0.75s;
+      background: var(--theme-button-color);
+      cursor: not-allowed;
+      text-shadow: none;
+      opacity: 0.83;
+    }
+  }
+</style>
