8e424f8465ebb5dd5d4dcf2fef7d782b5106e6e9
Matthew Elder <melder@hellotech.com>
2021-04-29 10:55:17 -0600

    if time val empty return empty string

--- api/lib/integrations/wyre/util.go
+++ api/lib/integrations/wyre/util.go
@@ -23,9 +23,17 @@ func WyreTransferToProto(t *Transfer) *proto.WyreTransfer {
 		Blockhash:      t.BlockchainTx.Blockhash,
 		NetworkTxId:    t.BlockchainTx.NetworkTxID,
 		Status:         t.Status,
-		CreatedAt:      time.Unix(t.CreatedAt/1000, 0).Format(time.RFC3339),
-		ExpiresAt:      time.Unix(t.ExpiresAt/1000, 0).Format(time.RFC3339),
-		CompletedAt:    time.Unix(t.CompletedAt/1000, 0).Format(time.RFC3339),
-		CancelledAt:    time.Unix(t.CancelledAt/1000, 0).Format(time.RFC3339),
+		CreatedAt:      formatEpochMSAsRFC3339(t.CreatedAt),
+		ExpiresAt:      formatEpochMSAsRFC3339(t.ExpiresAt),
+		CompletedAt:    formatEpochMSAsRFC3339(t.CompletedAt),
+		CancelledAt:    formatEpochMSAsRFC3339(t.CancelledAt),
 	}
+}
+
+func formatEpochMSAsRFC3339(epochMS int64) string {
+	if epochMS == 0 {
+		return ""
+	}
+
+	return time.Unix(epochMS/1000, 0).Format(time.RFC3339)
 }
