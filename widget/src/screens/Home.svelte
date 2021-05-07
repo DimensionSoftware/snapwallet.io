cffe326dfbdb8445e34ae39db1629ec9bc6b7b3a
Chris Almeida <thechrisalmeida@gmail.com>
2021-05-07 11:37:02 -0600

    update profile on mount

--- widget/src/screens/Home.svelte
+++ widget/src/screens/Home.svelte
@@ -243,6 +243,8 @@
     getInitialPrices()
     getNextPath()
     const interval = priceStore.pollPrices()
+    // Profile should be updated when user comes back here from any other route
+    userStore.fetchUserProfile()
     // TODO: @khoerling if ($configStore.intent === 'donate') {transactionStore.update({inMedium: TransactionMediums.DEBIT_CARD})}
     return () => clearInterval(interval)
   })
