495943aedb6e92171e6c108bec24afe59bbb5999
Chris Almeida <thechrisalmeida@gmail.com>
2021-05-12 16:26:20 -0600

    add card animation and copy

--- widget/src/screens/DebitCard2fa.svelte
+++ widget/src/screens/DebitCard2fa.svelte
@@ -95,9 +95,31 @@
         />
       </Label>
     {/if}
-    <!-- TODO: add an animation or something-->
     {#if !isOneCodeRequired}
-      Authorizing card...
+      <p style="padding: 1.5rem">
+        Please wait while we authorize your card. This will only take a minute.
+      </p>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <div class="cardholder-text">
+              <p style="text-transform:capitalize;">
+                **** **** **** {$debitCardStore.number.substring(
+                  $debitCardStore.number.length - 4,
+                )}
+                <br />
+                {$debitCardStore.firstName}
+                {$debitCardStore.lastName}
+              </p>
+            </div>
+            <div class="nfc-chip" />
+          </div>
+          <div class="flip-card-back">
+            <div class="flip-card-stripe" />
+            <div class="flip-card-signature" />
+          </div>
+        </div>
+      </div>
     {/if}
   </ModalBody>
   <ModalFooter>
@@ -111,4 +133,84 @@
 </ModalContent>
 
 <style lang="scss">
+  .flip-card {
+    height: 10rem;
+    width: 17.5rem;
+    perspective: 1000px;
+    backface-visibility: hidden;
+    border-radius: 0.5rem;
+    & > .flip-card-inner {
+      border-radius: 0.5rem;
+      animation: flipHorizontal 1s infinite;
+      position: relative;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      transition: transform 0.8s;
+      transform-style: preserve-3d;
+      & > .flip-card-front {
+        font-family: Courier, monospace;
+        background-color: red;
+        color: white;
+        border-radius: 0.5rem;
+        & > .nfc-chip {
+          position: absolute;
+          height: 2rem;
+          width: 3rem;
+          background-color: goldenrod;
+          top: 3rem;
+          left: 1.5rem;
+          border-radius: 0.2rem;
+        }
+        & > .cardholder-text {
+          position: absolute;
+          width: 100%;
+          top: 5rem;
+          left: 1rem;
+          text-align: left;
+        }
+      }
+
+      & > .flip-card-back {
+        border-radius: 0.5rem;
+        transform: rotateY(180deg);
+        background-color: red;
+        backface-visibility: hidden;
+        & > .flip-card-stripe {
+          background-color: black;
+          position: absolute;
+          top: 1.5rem;
+          width: 100%;
+          height: 20%;
+        }
+        & > .flip-card-signature {
+          position: absolute;
+          height: 15%;
+          width: 60%;
+          top: 4.5rem;
+          left: 1rem;
+          background-color: white;
+        }
+      }
+    }
+  }
+
+  /* Position the front and back of card face */
+  .flip-card-front,
+  .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+  }
+
+  @keyframes flipHorizontal {
+    from {
+      transform: rotateY(0deg);
+    }
+    to {
+      transform: rotateY(359deg);
+    }
+  }
 </style>
