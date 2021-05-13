e45117d361d8468ee372d0b8d4dc754481e218c0
Matthew Elder <melder@hellotech.com>
2021-05-12 23:04:23 -0600

    fix bug

--- api/lib/integrations/wyre/wyre.go
+++ api/lib/integrations/wyre/wyre.go
@@ -204,7 +204,7 @@ type GetWalletOrderReservationRequest struct {
 }
 
 type CreateWalletOrderRequest struct {
-	Firstname         string               `json:"givenName"`              // Card first name
+	FirstName         string               `json:"givenName"`              // Card first name
 	LastName          string               `json:"familyName"`             // Card last name
 	Email             string               `json:"email"`                  // User's email
 	PhoneNumber       string               `json:"phone"`                  // User's phone number (should match card)
