3e77103bcd67b0dad1d992a5d655a72ff41f4d98
Matthew Elder <melder@hellotech.com>
2021-06-03 23:55:18 -0600

    proof of concept test suite

---
+++ api/lib/server/util_test.go
@@ -0,0 +1,15 @@
+package server_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Util", func() {
+	Context("my milkshake", func() {
+		It("brings all the boys to the yard", func() {
+			Expect(1).To(Equal(1))
+		})
+	})
+
+})
