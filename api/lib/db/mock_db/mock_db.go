6fd55ac933378b94a4c1739083ace2c8f1a7f1a9
Matthew Elder <melder@hellotech.com>
2021-06-03 19:45:56 -0600

    update mocks

--- api/lib/db/mock_db/mock_db.go
+++ api/lib/db/mock_db/mock_db.go
@@ -8,6 +8,8 @@ import (
 	firestore "cloud.google.com/go/firestore"
 	context "context"
 	gomock "github.com/golang/mock/gomock"
+	gotoconfig "github.com/khoerling/flux/api/lib/db/models/gotoconfig"
+	job "github.com/khoerling/flux/api/lib/db/models/job"
 	onetimepasscode "github.com/khoerling/flux/api/lib/db/models/onetimepasscode"
 	usedrefreshtoken "github.com/khoerling/flux/api/lib/db/models/usedrefreshtoken"
 	user "github.com/khoerling/flux/api/lib/db/models/user"
@@ -15,6 +17,7 @@ import (
 	item "github.com/khoerling/flux/api/lib/db/models/user/plaid/item"
 	profiledata "github.com/khoerling/flux/api/lib/db/models/user/profiledata"
 	common "github.com/khoerling/flux/api/lib/db/models/user/profiledata/common"
+	transaction "github.com/khoerling/flux/api/lib/db/models/user/transaction"
 	account "github.com/khoerling/flux/api/lib/db/models/user/wyre/account"
 	paymentmethod "github.com/khoerling/flux/api/lib/db/models/user/wyre/paymentmethod"
 	reflect "reflect"
@@ -41,6 +44,36 @@ func NewMockDb(ctrl *gomock.Controller) *MockDb {
 // EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockDb) EXPECT() *MockDbMockRecorder {
 	return m.recorder
+}
+
+// SaveGotoConfig mocks base method
+func (m *MockDb) SaveGotoConfig(ctx context.Context, g *gotoconfig.Config) (gotoconfig.ShortID, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SaveGotoConfig", ctx, g)
+	ret0, _ := ret[0].(gotoconfig.ShortID)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// SaveGotoConfig indicates an expected call of SaveGotoConfig
+func (mr *MockDbMockRecorder) SaveGotoConfig(ctx, g interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveGotoConfig", reflect.TypeOf((*MockDb)(nil).SaveGotoConfig), ctx, g)
+}
+
+// GetGotoConfigByShortID mocks base method
+func (m *MockDb) GetGotoConfigByShortID(ctx context.Context, shortID gotoconfig.ShortID) (*gotoconfig.Config, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetGotoConfigByShortID", ctx, shortID)
+	ret0, _ := ret[0].(*gotoconfig.Config)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetGotoConfigByShortID indicates an expected call of GetGotoConfigByShortID
+func (mr *MockDbMockRecorder) GetGotoConfigByShortID(ctx, shortID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetGotoConfigByShortID", reflect.TypeOf((*MockDb)(nil).GetGotoConfigByShortID), ctx, shortID)
 }
 
 // CreateOneTimePasscode mocks base method
@@ -70,6 +103,79 @@ func (m *MockDb) SaveUser(ctx context.Context, tx *firestore.Transaction, u *use
 func (mr *MockDbMockRecorder) SaveUser(ctx, tx, u interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveUser", reflect.TypeOf((*MockDb)(nil).SaveUser), ctx, tx, u)
+}
+
+// SaveJob mocks base method
+func (m *MockDb) SaveJob(ctx context.Context, tx *firestore.Transaction, j *job.Job) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SaveJob", ctx, tx, j)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SaveJob indicates an expected call of SaveJob
+func (mr *MockDbMockRecorder) SaveJob(ctx, tx, j interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveJob", reflect.TypeOf((*MockDb)(nil).SaveJob), ctx, tx, j)
+}
+
+// SaveTransaction mocks base method
+func (m *MockDb) SaveTransaction(ctx context.Context, tx *firestore.Transaction, userID user.ID, transaction *transaction.Transaction) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SaveTransaction", ctx, tx, userID, transaction)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SaveTransaction indicates an expected call of SaveTransaction
+func (mr *MockDbMockRecorder) SaveTransaction(ctx, tx, userID, transaction interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SaveTransaction", reflect.TypeOf((*MockDb)(nil).SaveTransaction), ctx, tx, userID, transaction)
+}
+
+// GetTransactions mocks base method
+func (m *MockDb) GetTransactions(ctx context.Context, userID user.ID) (*transaction.Transactions, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetTransactions", ctx, userID)
+	ret0, _ := ret[0].(*transaction.Transactions)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetTransactions indicates an expected call of GetTransactions
+func (mr *MockDbMockRecorder) GetTransactions(ctx, userID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTransactions", reflect.TypeOf((*MockDb)(nil).GetTransactions), ctx, userID)
+}
+
+// GetTransactionByExternalId mocks base method
+func (m *MockDb) GetTransactionByExternalId(ctx context.Context, tx *firestore.Transaction, userID user.ID, externalID transaction.ExternalID) (*transaction.Transaction, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetTransactionByExternalId", ctx, tx, userID, externalID)
+	ret0, _ := ret[0].(*transaction.Transaction)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetTransactionByExternalId indicates an expected call of GetTransactionByExternalId
+func (mr *MockDbMockRecorder) GetTransactionByExternalId(ctx, tx, userID, externalID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTransactionByExternalId", reflect.TypeOf((*MockDb)(nil).GetTransactionByExternalId), ctx, tx, userID, externalID)
+}
+
+// GetJobByKindAndStatusAndRelatedId mocks base method
+func (m *MockDb) GetJobByKindAndStatusAndRelatedId(ctx context.Context, kind job.Kind, status job.Status, relatedID string) (*job.Job, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetJobByKindAndStatusAndRelatedId", ctx, kind, status, relatedID)
+	ret0, _ := ret[0].(*job.Job)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetJobByKindAndStatusAndRelatedId indicates an expected call of GetJobByKindAndStatusAndRelatedId
+func (mr *MockDbMockRecorder) GetJobByKindAndStatusAndRelatedId(ctx, kind, status, relatedID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetJobByKindAndStatusAndRelatedId", reflect.TypeOf((*MockDb)(nil).GetJobByKindAndStatusAndRelatedId), ctx, kind, status, relatedID)
 }
 
 // SaveFileMetadata mocks base method
@@ -132,18 +238,17 @@ func (mr *MockDbMockRecorder) GetUserByID(ctx, tx, userID interface{}) *gomock.C
 }
 
 // SavePlaidItem mocks base method
-func (m *MockDb) SavePlaidItem(ctx context.Context, userID user.ID, itemID item.ID, accessToken string, accountIDs []string) (*item.Item, error) {
+func (m *MockDb) SavePlaidItem(ctx context.Context, userID user.ID, item *item.Item) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "SavePlaidItem", ctx, userID, itemID, accessToken, accountIDs)
-	ret0, _ := ret[0].(*item.Item)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
+	ret := m.ctrl.Call(m, "SavePlaidItem", ctx, userID, item)
+	ret0, _ := ret[0].(error)
+	return ret0
 }
 
 // SavePlaidItem indicates an expected call of SavePlaidItem
-func (mr *MockDbMockRecorder) SavePlaidItem(ctx, userID, itemID, accessToken, accountIDs interface{}) *gomock.Call {
+func (mr *MockDbMockRecorder) SavePlaidItem(ctx, userID, item interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SavePlaidItem", reflect.TypeOf((*MockDb)(nil).SavePlaidItem), ctx, userID, itemID, accessToken, accountIDs)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SavePlaidItem", reflect.TypeOf((*MockDb)(nil).SavePlaidItem), ctx, userID, item)
 }
 
 // SaveWyreAccount mocks base method
@@ -394,4 +499,19 @@ func (m *MockDb) CleanAgedPasscodes(ctx context.Context) (int, error) {
 func (mr *MockDbMockRecorder) CleanAgedPasscodes(ctx interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CleanAgedPasscodes", reflect.TypeOf((*MockDb)(nil).CleanAgedPasscodes), ctx)
+}
+
+// GetUserByWyreAccountID mocks base method
+func (m *MockDb) GetUserByWyreAccountID(ctx context.Context, wyreAccountID account.ID) (*user.User, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetUserByWyreAccountID", ctx, wyreAccountID)
+	ret0, _ := ret[0].(*user.User)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetUserByWyreAccountID indicates an expected call of GetUserByWyreAccountID
+func (mr *MockDbMockRecorder) GetUserByWyreAccountID(ctx, wyreAccountID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetUserByWyreAccountID", reflect.TypeOf((*MockDb)(nil).GetUserByWyreAccountID), ctx, wyreAccountID)
 }
