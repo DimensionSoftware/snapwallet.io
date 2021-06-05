c5d5f3157113860cd6e962d6c2c15c5133ab940e
Matthew Elder <melder@hellotech.com>
2021-06-05 17:11:52 -0600

    test: CreateOneTimePasscode

--- api/lib/db/firebase_db/firebase_db_test.go
+++ api/lib/db/firebase_db/firebase_db_test.go
@@ -2,8 +2,10 @@ package firebase_db_test
 
 import (
 	"context"
+	"time"
 
 	"github.com/khoerling/flux/api/lib/db/models/gotoconfig"
+	"github.com/khoerling/flux/api/lib/db/models/onetimepasscode"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -37,7 +39,7 @@ var _ = Describe("FirebaseDb", func() {
 		})
 
 		It("new ID returns ShortID", func() {
-			returnedShortID, err := testManager.SaveGotoConfig(context.Background(), &gotoconfig.Config{
+			returnedShortID, err := testManager.Db.SaveGotoConfig(context.Background(), &gotoconfig.Config{
 				ID:      id,
 				ShortID: shortID,
 				Config:  config,
@@ -55,7 +57,7 @@ var _ = Describe("FirebaseDb", func() {
 			})
 			Expect(err).ShouldNot(HaveOccurred())
 
-			returnedShortID, err := testManager.SaveGotoConfig(context.Background(), &gotoconfig.Config{
+			returnedShortID, err := testManager.Db.SaveGotoConfig(context.Background(), &gotoconfig.Config{
 				ID:      id,
 				ShortID: gotoconfig.NewShortID(),
 				Config:  config,
@@ -68,7 +70,7 @@ var _ = Describe("FirebaseDb", func() {
 
 	Context("GetGotoConfigByShortID", func() {
 		It("returns nil when not found", func() {
-			config, err := testManager.GetGotoConfigByShortID(context.Background(), gotoconfig.NewShortID())
+			config, err := testManager.Db.GetGotoConfigByShortID(context.Background(), gotoconfig.NewShortID())
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(config).Should(BeNil())
 		})
@@ -95,7 +97,7 @@ var _ = Describe("FirebaseDb", func() {
 				Config:  config,
 			}
 
-			_, err = testManager.SaveGotoConfig(context.Background(), &gotoConfig)
+			_, err = testManager.Db.SaveGotoConfig(context.Background(), &gotoConfig)
 			Expect(err).ShouldNot(HaveOccurred())
 
 			conf, err := testManager.GetGotoConfigByShortID(context.Background(), shortID)
@@ -104,6 +106,49 @@ var _ = Describe("FirebaseDb", func() {
 
 			Expect(*conf).Should(Equal(gotoConfig))
 		})
+	})
+	Context("CreateOneTimePasscode", func() {
+		Context("with email", func() {
+			var otp *onetimepasscode.OneTimePasscode
+			BeforeEach(func() {
+				var err error
+				otp, err = testManager.Db.CreateOneTimePasscode(context.Background(), "bob@microsoft.com", onetimepasscode.LoginKindEmail)
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("returns six digits", func() {
+				Expect(otp.Code).Should(HaveLen(6))
+			})
+
+			It("should return login kind email", func() {
+				Expect(otp.Kind).Should(Equal(onetimepasscode.LoginKindEmail))
+			})
+
+			It("should have a created_at timestamp which is recent", func() {
+				Expect(otp.CreatedAt).Should(BeTemporally("~", time.Now(), time.Second))
+			})
+		})
+		Context("with phone", func() {
+			var otp *onetimepasscode.OneTimePasscode
+			BeforeEach(func() {
+				var err error
+				otp, err = testManager.Db.CreateOneTimePasscode(context.Background(), "818-900-3454", onetimepasscode.LoginKindPhone)
+				Expect(err).ShouldNot(HaveOccurred())
+			})
+
+			It("returns six digits", func() {
+				Expect(otp.Code).Should(HaveLen(6))
+			})
+
+			It("should return login kind phone", func() {
+				Expect(otp.Kind).Should(Equal(onetimepasscode.LoginKindPhone))
+			})
+
+			It("should have a created_at timestamp which is recent", func() {
+				Expect(otp.CreatedAt).Should(BeTemporally("~", time.Now(), time.Second))
+			})
+		})
+
 	})
 
 })
