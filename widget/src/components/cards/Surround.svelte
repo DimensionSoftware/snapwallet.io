b3c334c85746ce43fcf8e2903949fb7a6b64f5b3
Keith Hörling <keith@Slaptop-2.local>
2021-06-09 09:04:02 -0700

    add <Surround />

---
+++ widget/src/components/cards/Surround.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div class="surround" on:click={() => {}}>
+  <slot />
+</div>
+
+<style lang="scss">
+  @import '../../styles/_vars.scss';
+  @import '../../styles/animations.scss';
+
+  .surround {
+    position: relative;
+    padding: 0;
+    margin-top: 1rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 1rem 0.5rem;
+    border: 2px solid var(--theme-color);
+    border-radius: 0.75rem;
+  }
+</style>
