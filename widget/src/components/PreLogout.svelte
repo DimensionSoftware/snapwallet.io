1e8bd265cec5e38301f6226e8c58fd9dd0bd8e8b
Keith Hörling <keith@Slaptop-2.local>
2021-06-11 22:35:29 -0700

    pre-logout++

--- widget/src/components/PreLogout.svelte
+++ widget/src/components/PreLogout.svelte
@@ -6,6 +6,8 @@
   import { userStore } from '../stores/UserStore'
   import { transactionStore } from '../stores/TransactionStore'
   import { paymentMethodStore } from '../stores/PaymentMethodStore'
+  import Surround from './cards/Surround.svelte'
+  import Button from './Button.svelte'
 
   export let isVisible: boolean = false
   export let onClosed: Function
@@ -80,17 +82,17 @@
   on:mousedown={shouldClose}
   out:scale={{ opacity: 0, start: 1.15, duration: 175, easing: expoOut }}
 >
-  <article>
+  <Surround>
     <h3>Stay Signed In?</h3>
     <p>
       You will be securely signed out in <big>{expiresAfter}</big>
       {expiresAfterResolution}
     </p>
-    <div class="flex">
-      <button on:mousedown={logout} class="logout">Logout</button>
-      <button on:mousedown={cont} class="continue">Continue</button>
-    </div>
-  </article>
+  </Surround>
+  <div class="flex">
+    <Button on:mousedown={cont} class="continue">Continue</Button>
+    <button on:mousedown={logout} class="logout">Logout</button>
+  </div>
 </div>
 
 <style lang="scss">
@@ -102,44 +104,42 @@
     right: 0;
     bottom: 0;
     left: 0;
+    padding: 5rem 3rem;
     font-size: 1.2rem;
     z-index: 101;
     background: rgba(200, 200, 200, 0.7);
-    article {
-      position: relative;
-      margin: 0;
-      top: 20%;
-      background: var(--theme-modal-background);
-      border-radius: 3px;
-      padding: 1.5rem;
-      animation: slideUp 0.3s var(--theme-ease-out-back);
-      h3 {
-        margin: 0;
+    :global(.surround) {
+      padding: 0.5rem 2rem;
+    }
+    h3 {
+      margin-bottom: 0.25rem;
+    }
+    p {
+      font-size: 0.9rem;
+      margin: 0.24rem 0 1.75rem;
+    }
+    .flex {
+      margin-top: 1rem;
+      display: flex;
+      justify-content: start;
+      flex-direction: column;
+      .logout {
+        cursor: pointer;
+        font-size: 1rem;
+        width: 100%;
+        margin-top: 0.5rem;
+        color: var(--theme-text-color);
+        background: transparent;
+        border: none;
       }
-      p {
-        font-size: 0.9rem;
-        margin: 0.24rem 0 1.75rem;
-      }
-      .flex {
-        display: flex;
-        justify-content: start;
-        .continue {
-          cursor: pointer;
-          font-size: 1rem;
-          margin-left: 0.25rem;
-          color: var(--theme-text-color);
-          background: transparent;
-          border: none;
-        }
-        .logout {
-          cursor: pointer;
-          margin-right: 0.5rem;
-          background: var(--theme-color);
-          font-weight: 500;
-          padding: 0.4rem 1rem;
-          color: var(--theme-color-inverse);
-          border: none;
-        }
+      .continue {
+        cursor: pointer;
+        margin-right: 0.5rem;
+        background: var(--theme-color);
+        font-weight: 500;
+        padding: 0.4rem 1rem;
+        color: var(--theme-color-inverse);
+        border: none;
       }
     }
   }
