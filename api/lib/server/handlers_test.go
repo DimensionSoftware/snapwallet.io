4259ad1cb1ea4f0fe22dece384163abf2cd208d2
Matthew Elder <melder@hellotech.com>
2021-07-20 20:32:55 -0600

    wip

--- api/lib/server/handlers_test.go
+++ api/lib/server/handlers_test.go
@@ -2,13 +2,16 @@ package server_test
 
 import (
 	"context"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/khoerling/flux/api/lib/db/mock_db"
+	wyre_account "github.com/khoerling/flux/api/lib/db/models/user/wyre/account"
 	"github.com/khoerling/flux/api/lib/db/test_utils"
 	"github.com/khoerling/flux/api/lib/integration_t_manager/wire"
 	"github.com/khoerling/flux/api/lib/protocol"
 	"github.com/khoerling/flux/api/lib/server"
+	"github.com/lithammer/shortuuid/v3"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"google.golang.org/grpc/metadata"
@@ -29,12 +32,27 @@ var _ = Describe("Handlers", func() {
 	Context("WyreCreateTransfer", func() {
 		It("happy path", func() {
 			Skip("WIP")
+			now := time.Now()
 
 			mockDb := s.Db.(*mock_db.MockDb)
+			//mockWyre := s.Wyre.(*mock_wyre.MockClientInterface)
 			user := test_utils.GenFakeUser()
+			//wyreAccount := test_utils.GenFakeWyreAccount()
+			wyreAccount := &wyre_account.Account{
+				ID:        wyre_account.ID(shortuuid.New()),
+				APIKey:    "key",
+				SecretKey: "secret",
+				Status:    "APPROVED",
+				CreatedAt: now,
+				UpdatedAt: &now,
+			}
 			ctx := metadata.NewIncomingContext(ctx, map[string][]string{"user-id": {string(user.ID)}})
 
-			mockDb.EXPECT().GetUserByID(gomock.Any(), gomock.Any(), user.ID).Return(user, nil)
+			mockDb.EXPECT().GetUserByID(ctx, gomock.Any(), user.ID).Return(user, nil)
+			mockDb.EXPECT().GetWyreAccounts(ctx, gomock.Any(), user.ID).Return(
+				[]*wyre_account.Account{wyreAccount},
+				nil,
+			)
 
 			req := &protocol.WyreCreateTransferRequest{}
 			resp, err := s.WyreCreateTransfer(ctx, req)
