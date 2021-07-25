92fca6a68808788acb7409fadc1bc87ecf6489ad
Keith Hörling <keith@Slaptop-2.local>
2021-07-25 13:34:32 -0700

    fix: don't crash if input isn't masked

--- widget/src/components/inputs/Input.svelte
+++ widget/src/components/inputs/Input.svelte
@@ -46,13 +46,15 @@
       if (mask) return isValidKeyForMask(e, mask, defaultValue)
     }}
     on:input={e => {
-      // last chance to fix inputs
-      const val = e.target.value,
-        isValLongerThanMask = val.length > mask.length
-      // truncate if necessary
-      if (isValLongerThanMask) e.target.value = val.substr(0, val.length - 1)
-      // force correct format
-      e.target.value = withMaskOnInput(e.target.value, mask)
+      if (mask) {
+        // last chance to fix inputs
+        const val = e.target.value,
+          isValLongerThanMask = val.length > mask.length
+        // truncate if necessary
+        if (isValLongerThanMask) e.target.value = val.substr(0, val.length - 1)
+        // force correct format
+        e.target.value = withMaskOnInput(e.target.value, mask)
+      }
     }}
     on:keydown
     on:click
