e47b41dc1a8cd96bc39efadb295108ee639e3fa5
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-11 21:57:15 -0600

    drop in plug

--- api_2/routes/v1/index.js
+++ api_2/routes/v1/index.js
@@ -1,7 +1,13 @@
 const Router = require('koa-router'),
   router = new Router(),
   walletsRouter = require('./wallets'),
-  transferRouter = require('./transfer')
+  transferRouter = require('./transfer'),
+  { verifyJWTPlug } = require('../../middleware/auth')
+
+/**
+ * Authenticate client
+ */
+router.use(verifyJWTPlug)
 
 router.use('/wallets', walletsRouter.routes(), walletsRouter.allowedMethods())
 
