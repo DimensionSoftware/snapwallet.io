d784671d12fb5cc0dbafaa2f4309258cd357cc07
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-13 12:23:11 -0600

    add business creation route

---
+++ api_2/routes/v1/admin.js
@@ -0,0 +1,25 @@
+const KoaRouter = require('koa-router')
+const { v4 } = require('uuid')
+const Wyre = require('../../clients/wyre')
+const router = new KoaRouter()
+const { createBusiness } = require('../../db')
+
+router.post('/business', async (ctx, _next) => {
+  const { name } = ctx.request.body
+  const wyre = new Wyre()
+  const { data: wallet } = await wyre.createWallet()
+  const business = await createBusiness({
+    name,
+    wallet: {
+      depositAddresses: wallet.depositAddresses,
+      id: wallet.id,
+      name: wallet.name,
+      processor: 'wyre',
+      type: wallet.type,
+    },
+    apiKey: v4(),
+  })
+  ctx.body = { data: business }
+})
+
+module.exports = router
