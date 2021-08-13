65b986a7a066fd847793b700fe690d365d215eee
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-13 12:22:36 -0600

    add db methods

--- api_2/db.js
+++ api_2/db.js
@@ -9,6 +9,8 @@ admin.initializeApp({
 
 const collections = {
   users: 'users',
+  businesses: 'businesses',
+  events: 'events',
 }
 
 const db = admin.firestore()
@@ -22,4 +24,51 @@ const listUsers = () =>
       return ss.map((d) => d.data())
     })
 
-module.exports = { listUsers }
+const createBusiness = async ({ name, apiKey, wallet }) => {
+  const ref = await db.collection(collections.businesses).add({
+    apiKey,
+    name,
+    wallet,
+  })
+  const doc = await ref.get()
+  return { ...doc.data(), id: ref.id }
+}
+
+const createEvent = async ({ type, meta }) => {
+  const ref = await db.collection(collections.events).add({
+    type,
+    meta,
+  })
+  const doc = await ref.get()
+  return { ...doc.data(), id: ref.id }
+}
+
+const getBusinessByAPIKey = async (apiKey) => {
+  const result = await db
+    .collection(collections.businesses)
+    .where('apiKey', '==', apiKey)
+    .limit(1)
+    .get()
+
+  const doc = result.docs[0]
+  if (!doc) return
+  return { ...doc.data(), id: doc.id }
+}
+
+const getEvent = async (source) => {
+  const result = await db
+    .collection(collections.events)
+    .where('meta.source', '==', source)
+    .limit(1)
+    .get()
+  if (!result.docs[0]) return
+  return result.docs[0].data()
+}
+
+module.exports = {
+  listUsers,
+  createBusiness,
+  createEvent,
+  getBusinessByAPIKey,
+  getEvent,
+}
