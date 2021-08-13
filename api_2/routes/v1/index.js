e5236ea6839d44afb839a4af8f41fea704472d29
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-13 12:23:50 -0600

    update routes

--- api_2/routes/v1/index.js
+++ api_2/routes/v1/index.js
@@ -2,12 +2,23 @@ const Router = require('koa-router'),
   router = new Router(),
   walletsRouter = require('./wallets'),
   transferRouter = require('./transfer'),
+  adminRouter = require('./admin'),
+  webhooksRouter = require('./webhooks'),
   { verifyJWTPlug } = require('../../middleware/auth')
+
+router.use(
+  '/webhooks',
+  webhooksRouter.routes(),
+  webhooksRouter.allowedMethods()
+)
 
 /**
  * Authenticate client
  */
 router.use(verifyJWTPlug)
+
+// TODO: make sure this route is only accessible by admins
+router.use('/admin', adminRouter.routes(), adminRouter.allowedMethods())
 
 router.use('/wallets', walletsRouter.routes(), walletsRouter.allowedMethods())
 
