34954e556c426752b90a45160baa0b6c953f986a
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-14 10:38:47 -0600

    make entity required

--- api_2/schemas/event.js
+++ api_2/schemas/event.js
@@ -1,26 +1,25 @@
-const Joi =  require('joi'),
+const Joi = require('joi'),
   FlakeID = require('flake-idgen'),
-  Base58 = require("base-58")
+  Base58 = require('base-58')
 
 const EventSchema = Joi.object({
   id: Joi.string().default(newEventID),
   kind: Joi.string().required(),
   action: Joi.string(),
   data: Joi.any().required(),
-  entity: {
+  entity: Joi.object({
     kind: Joi.string().valid('BUSINESS', 'USER').required(),
     id: Joi.string().required(),
-  },
+  }).required(),
   recorded_at: Joi.date().iso().default(new Date().toISOString()),
 })
 
 // id can be 10 bits (or datacenter/worker can be used) .. in future -- not needed now
-const idGenerator = new FlakeID({ id: 0})
+const idGenerator = new FlakeID({ id: 0 })
 
 // DO NOT CHANGE THE BASE ENCODING!!! .. will mess up ordering
 function newEventID() {
   return Base58.encode(idGenerator.next())
 }
-
 
 module.exports = { EventSchema, newEventID }
