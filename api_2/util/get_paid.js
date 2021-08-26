20a38287c5814be7590ce8d5c1eb99f799139d7a
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-26 09:41:30 -0600

    look to events before processing each step

--- api_2/util/get_paid.js
+++ api_2/util/get_paid.js
@@ -1,10 +1,64 @@
 const Wyre = require('../clients/wyre')
 const { UnprocessableEntityError } = require('../error'),
-  { createEvent, JOB_PUBLISHER } = require('../db')
+  { createEvent, JOB_PUBLISHER, EVENTS } = require('../db'),
+  { EVENT_KINDS } = require('../constants')
 
-const payoutTask = async (data) => {
+/**
+ * Transfer a percentage of the transaction funds
+ * to our internal business wallet.
+ */
+const processInternalBusinessTransaction = async ({ params, userId }) => {
+  try {
+    const wyre = new Wyre()
+    await wyre.createTransfer(params)
+    await createEvent({
+      type: EVENT_KINDS.transferred_to_internal_wallet,
+      meta: params,
+      entity: { id: userId, kind: 'USER' },
+    })
+  } catch (e) {
+    throw e
+  }
+}
+
+/**
+ * Transfer transaction funds to our business customer.
+ */
+const processExternalBusinessTransaction = async ({ params, userId }) => {
+  try {
+    const wyre = new Wyre()
+    await wyre.createTransfer(params)
+    await createEvent({
+      type: EVENT_KINDS.transferred_to_internal_wallet,
+      meta: params,
+      entity: { id: userId, kind: 'USER' },
+    })
+  } catch (e) {
+    throw e
+  }
+}
+
+const payoutTask = async (data, logger) => {
   try {
     const userId = data.userId
+    const events = await EVENTS.listByEntityID(userId)
+
+    const { internalTxnRequired, externalTxnRequired } = events.reduce(
+      (acc, e) => {
+        if (e.kind === EVENT_KINDS.transferred_to_business_customer)
+          return { ...acc, externalTxnRequired: false }
+        if (e.kind === EVENT_KINDS.transferred_to_internal_wallet)
+          return { ...acc, internalTxnRequired: false }
+
+        return acc
+      },
+      { internalTxnRequired: true, externalTxnRequired: true }
+    )
+
+    if (!internalTxnRequired && !externalTxnRequired) {
+      logger.info({ msg: 'Transaction already processed. Skipping.' })
+    }
+
     const wyre = new Wyre()
     const wallet = await wyre.getWallet({ id: data.source })
     const walletBalances = wallet.availableBalances
@@ -29,34 +83,25 @@ const payoutTask = async (data) => {
       source: `wallet:${data.source}`,
     }
 
-    const internalBusinessTxn = {
-      ...baseParams,
-      sourceAmount: swAmount,
-      dest: `wallet:${process.env.SNAP_WALLET_WYRE_SAVINGS_WALLET}`,
+    // Collect our percentage
+    if (internalTxnRequired) {
+      const params = {
+        ...baseParams,
+        sourceAmount: swAmount,
+        dest: `wallet:${process.env.SNAP_WALLET_WYRE_SAVINGS_WALLET}`,
+      }
+      await processInternalBusinessTransaction({ params, userId })
     }
 
-    // NOTE: make idempotent
-    await wyre.createTransfer(internalBusinessTxn)
-
-    await createEvent({
-      type: 'transferred_to_internal_wallet',
-      meta: internalBusinessTxn,
-      entity: { id: userId, kind: 'USER' },
-    })
-
-    const externalBusinessTxn = {
-      ...baseParams,
-      sourceAmount: remainingAmount,
-      dest: `wallet:${data.destination}`,
+    // Transfer remaining to our business customer
+    if (externalTxnRequired) {
+      const params = {
+        ...baseParams,
+        sourceAmount: remainingAmount,
+        dest: `wallet:${data.destination}`,
+      }
+      await processExternalBusinessTransaction({ params, userId })
     }
-
-    await wyre.createTransfer(externalBusinessTxn)
-
-    await createEvent({
-      type: 'transferred_to_business_customer',
-      meta: externalBusinessTxn,
-      entity: { id: userId, kind: 'USER' },
-    })
   } catch (e) {
     await JOB_PUBLISHER.publish({ worker: 'payoutTask', config: data })
     throw e
