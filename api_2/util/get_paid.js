c7519b670ebb9e3044552b19c2c9315104effe5b
Chris Almeida <thechrisalmeida@gmail.com>
2021-08-26 09:18:36 -0600

    add validation and event tracking

--- api_2/util/get_paid.js
+++ api_2/util/get_paid.js
@@ -1,11 +1,24 @@
 const Wyre = require('../clients/wyre')
+const { UnprocessableEntityError } = require('../error'),
+  { createEvent } = require('../db')
 
 const payoutTask = async (data) => {
   try {
+    const userId = data.userId
     const wyre = new Wyre()
+    const wallet = await wyre.getWallet({ id: data.source })
+    const walletBalances = wallet.availableBalances
+    // Any currency not in the map will fall under ERC20 so fallback to eth address
+    const walletBalance =
+      walletBalances[data.sourceCurrency] || walletBalances['ETH']
+
+    if (!walletBalance || walletBalance < data.sourceAmount) {
+      throw new UnprocessableEntityError(
+        `Wallet has insufficient funds for processing.`
+      )
+    }
 
     // TODO: figure out what fee should be and take from environment.
-    // TODO: handle case where there's an amount left over (user sent too much)
     const swAmount = 0.0025 * data.sourceAmount
     const remainingAmount = data.sourceAmount - swAmount
     const baseParams = {
@@ -13,22 +26,37 @@ const payoutTask = async (data) => {
       autoConfirm: true,
       sourceCurrency: data.sourceCurrency,
       destCurrency: data.destCurrency,
+      source: `wallet:${data.source}`,
     }
 
-    return await Promise.all([
-      wyre.createTransfer({
-        ...baseParams,
-        source: `wallet:${data.source}`,
-        sourceAmount: swAmount,
-        dest: `wallet:${process.env.SNAP_WALLET_WYRE_SAVINGS_WALLET}`,
-      }),
-      wyre.createTransfer({
-        ...baseParams,
-        source: `wallet:${data.source}`,
-        sourceAmount: remainingAmount,
-        dest: `wallet:${data.destination}`,
-      }),
-    ])
+    const internalBusinessTxn = {
+      ...baseParams,
+      sourceAmount: swAmount,
+      dest: `wallet:${process.env.SNAP_WALLET_WYRE_SAVINGS_WALLET}`,
+    }
+
+    // NOTE: make idempotent
+    await wyre.createTransfer(internalBusinessTxn)
+
+    await createEvent({
+      type: 'transferred_to_internal_wallet',
+      meta: internalBusinessTxn,
+      entity: { id: userId, kind: 'USER' },
+    })
+
+    const externalBusinessTxn = {
+      ...baseParams,
+      sourceAmount: remainingAmount,
+      dest: `wallet:${data.destination}`,
+    }
+
+    await wyre.createTransfer(externalBusinessTxn)
+
+    await createEvent({
+      type: 'transferred_to_business_customer',
+      meta: externalBusinessTxn,
+      entity: { id: userId, kind: 'USER' },
+    })
   } catch (e) {
     throw e
   }
