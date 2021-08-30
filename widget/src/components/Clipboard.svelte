1db53b96ba76e8543d0d2ca5724a17528d3062a2
Keith Hörling <keith@Slaptop-Sr.local>
2021-08-29 19:10:57 -0700

    adds copy-to-clipboard

---
+++ widget/src/components/Clipboard.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { tick } from 'svelte'
+
+  export let value = null
+  let hiddenInput
+
+  async function copy() {
+    // copy-to-clipboard
+    // - TODO there are more portable ways
+    await tick()
+    hiddenInput.focus()
+    hiddenInput.select()
+    try {
+      if (!document.execCommand('copy'))
+        console.warn(`Unable to copy: ${value}`)
+    } catch (err) {
+      console.warn(`Error copying "${value}": ${err}`)
+    }
+  }
+</script>
+
+{#if value != null}
+  <textarea bind:this={hiddenInput}>{value}</textarea>
+{/if}
+<svg
+  on:click={copy}
+  title="Copy to clipboard"
+  class="octicon octicon-clippy"
+  viewBox="0 0 14 16"
+  version="1.1"
+  width="20"
+  height="22"
+  aria-hidden="true"
+>
+  <path
+    fill-rule="evenodd"
+    d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"
+  />
+</svg>
+
+<style>
+  textarea {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    box-shadow: none;
+    background: transparent;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+</style>
