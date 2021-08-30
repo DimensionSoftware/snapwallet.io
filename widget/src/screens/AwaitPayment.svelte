e754b4dd4961bcfc3d8a88728765925712501df9
Keith Hörling <keith@Slaptop-Sr.local>
2021-08-29 19:15:01 -0700

    wip: AwaitPayment
    - fix: github actions

--- widget/src/screens/AwaitPayment.svelte
+++ widget/src/screens/AwaitPayment.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { faClipboard } from '@fortawesome/free-solid-svg-icons'
+  import { faClipboard, faArrowDown } from '@fortawesome/free-solid-svg-icons'
   import FaIcon from 'svelte-awesome'
   import { CryptoIcons, formatLocaleCurrency, dropEndingZeros } from '../util'
   import { TransactionIntents, TransactionMediums } from '../types'
@@ -12,8 +12,11 @@
   import ModalContent from '../components/ModalContent.svelte'
   import ModalBody from '../components/ModalBody.svelte'
   import ModalHeader from '../components/ModalHeader.svelte'
-  import Surround from './cards/Surround.svelte'
+  import Surround from '../components/cards/Surround.svelte'
   import Button from '../components/Button.svelte'
+  import Clipboard from '../components/Clipboard.svelte'
+
+  const dstCurrency = $transactionStore.destinationCurrency?.toString()
 
   onMount(() => {
     // TODO build-in
@@ -25,7 +28,23 @@
   <ModalBody>
     <Surround>
       <h3>Send</h3>
-      <h4>Send</h4>
+      <div class="row">
+        <div class="crypto-icon">
+          <svelte:component
+            this={CryptoIcons[
+              $transactionStore.destinationCurrency?.toString().toUpperCase() ??
+                'BTC'
+            ]}
+          />
+        </div>
+        <h4>
+          {dstCurrency}
+          {dstCurrency}
+        </h4>
+        <Clipboard value="foo" />
+      </div>
+      <FaIcon data={faArrowDown} />
+      <span>qrcode</span>
       <strong>Or, Copy & Paste</strong>
     </Surround>
   </ModalBody>
@@ -34,4 +53,10 @@
 <style lang="scss">
   @import '../styles/_vars.scss';
   @import '../styles/animations.scss';
+  .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
