fba8ca3c058361eee9d50274d62c5f967716e3a7
Keith Hörling <keith@Slaptop-Sr.local>
2021-09-01 22:45:45 -0700

    wip: <AwaitPayment />

--- widget/src/screens/AwaitPayment.svelte
+++ widget/src/screens/AwaitPayment.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { faClipboard, faArrowDown } from '@fortawesome/free-solid-svg-icons'
+  import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
   import FaIcon from 'svelte-awesome'
+  // @ts-ignore
+  import QR from 'qr-creator'
   import { CryptoIcons, formatLocaleCurrency, dropEndingZeros } from '../util'
   import { TransactionIntents, TransactionMediums } from '../types'
   import { push } from 'svelte-spa-router'
@@ -16,34 +18,45 @@
   import Button from '../components/Button.svelte'
   import Clipboard from '../components/Clipboard.svelte'
 
-  const dstCurrency = $transactionStore.destinationCurrency?.toString(),
-    dstAmount = $transactionStore.destinationAmount,
-    dstAddress = '0xDEADBEEF'
+  // TODO remove demo defaults and use real values
+  const dstCurrency = 'BTC', //$transactionStore.destinationCurrency.name ?? 'BTC',
+    dstAmount = $transactionStore.destinationAmount ?? 69420,
+    dstAddress = '0xDEADBEEF',
+    Icon = CryptoIcons[dstCurrency]
 
   onMount(() => {
     // TODO build-in
+    QR.render(
+      {
+        dstAddress,
+        radius: 0.0, // 0.0 to 0.5
+        ecLevel: 'H', // L, M, Q, H
+        fill: '#111',
+        size: 128, // in pixels
+      },
+      document.getElementById('qrcode'),
+    )
   })
 </script>
 
 <ModalContent>
   <ModalHeader>Awaiting Payment</ModalHeader>
   <ModalBody>
+    <div class="glow" />
     <Surround>
       <h2>Send</h2>
       <div class="row">
         <div class="crypto-icon">
-          <svelte:component
-            this={CryptoIcons[dstCurrency.toUpperCase() ?? 'BTC']}
-          />
+          <Icon size="30" height="30" width="30" viewBox="-4 0 40 40" />
         </div>
         <h4>
-          {dstAmount}
+          {formatLocaleCurrency(dstCurrency, dstAmount)}
         </h4>
         <Clipboard value={dstAmount} />
       </div>
       <FaIcon class="down-arrow" data={faArrowDown} />
-      <span>qrcode</span>
-      <strong>Or, Copy & Paste</strong>
+      <div id="qrcode" class="qrcode" title="Scan to Send Payment" />
+      <small>Or, Copy & Paste</small>
       <div class="row">
         <p>{dstAddress}</p>
         <Clipboard value={dstAddress} />
@@ -62,10 +75,80 @@
   :global(.down-arrow) {
     align-self: center;
   }
+  .glow {
+    box-shadow: 0 0 0 0 rgba(var(--theme-button-glow-color), 0.5);
+    animation: glow 1.5s linear;
+    animation-iteration-count: infinite;
+    border-radius: 100%;
+    height: 4px;
+    width: 4px;
+    background: transparent;
+    margin: 0.25rem auto;
+    &:before {
+      position: absolute;
+      content: '';
+      width: 4px;
+      left: -1px;
+      top: -1px;
+      height: 4px;
+      border-radius: 100%;
+      border: 1px solid var(--theme-color);
+      opacity: 0.5;
+    }
+  }
+  h2 {
+    font-size: 1.2rem;
+  }
   .row {
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    font-size: 1.1rem;
+    grid-gap: 0.5rem;
+    margin-bottom: 0.25rem;
+    .crypto-icon {
+      margin-top: 7px;
+    }
+    h4 {
+      margin: 0;
+    }
+  }
+  .qrcode {
+    margin: 1rem auto;
+  }
+  small {
+    margin: 1.5rem 0 0.5rem 0;
+    font-weight: 600;
+    position: relative;
+    &:after,
+    &:before {
+      position: absolute;
+      content: '';
+      top: 10px;
+      width: 20px;
+      height: 1px;
+      opacity: 0.5;
+    }
+    &:after {
+      margin-left: 5px;
+      background-image: linear-gradient(
+        to left,
+        transparent,
+        var(--theme-color)
+      );
+    }
+    &:before {
+      margin-left: -25px;
+      background-image: linear-gradient(
+        to right,
+        transparent,
+        var(--theme-color)
+      );
+    }
+  }
+  p {
+    margin: 0;
+    font-size: 0.8rem;
   }
 </style>
