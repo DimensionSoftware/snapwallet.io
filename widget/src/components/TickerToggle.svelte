151247f92b11b18ccb7fd2bb1e57893d5fc176ce
keith <keith@Slaptop-Sr.local>
2021-12-27 21:58:23 -0800

    adds <TickerToggle/> for buy/sell toggle

---
+++ widget/src/components/TickerToggle.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { expoOut } from 'svelte/easing'
+
+  import { blur } from 'svelte/transition'
+
+  export let ticker: string = 'USD'
+  export let isBuy: boolean = true
+</script>
+
+<div class="left" title={`Buy ${ticker}`}>
+  {#if isBuy}
+    Buy
+    <small in:blur={{ opacity: 0.5, easing: expoOut, duration: 2000 }}
+      >Sell</small
+    >
+  {:else}
+    Sell
+    <small in:blur={{ opacity: 0.5, easing: expoOut, duration: 2000 }}
+      >Buy</small
+    >
+  {/if}
+</div>
+
+<style lang="scss">
+  @import '../styles/_vars.scss';
+  .left {
+    text-align: left;
+  }
+  small {
+    margin: 0 0 0 0.7rem;
+    opacity: 0.75;
+  }
+</style>
